906a2ca5c84fe1e44e5e16561047ea7c00ae5bac
Ronald Lima <ronaldazevedolima@gmail.com>
2020-08-25 18:52:45 -0300

    O que é isso? Botão?

---
+++ desafio7.sql
@@ -0,0 +1,17 @@
+SELECT
+(
+SELECT
+UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME))
+FROM hr.employees
+WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+) AS `Nome completo`,
+START_DATE AS `Data de início`,
+(
+SELECT
+SALARY
+FROM hr.employees
+WHERE employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+) AS `Salário`
+FROM hr.job_history
+WHERE MONTH(START_DATE) = 1 OR MONTH(START_DATE) = 2 OR MONTH(START_DATE) = 3
+ORDER BY `Nome completo`, `Data de início`;
