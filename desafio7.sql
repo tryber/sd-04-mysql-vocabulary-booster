880dd39f5764eb65ba0d0eb4f603b160bdedbe4b
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-25 14:44:22 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT UCASE(CONCAT(em.first_name, ' ', em.last_name)) AS 'Nome Completo',
+hs.start_date AS 'Data de início',
+em.salary AS 'Salário'
+FROM hr.employees AS em
+INNER JOIN hr.job_history AS hs
+ON hs.EMPLOYEE_ID = em.EMPLOYEE_ID
+WHERE MONTH(hs.START_DATE) IN ( 01,02,03 )
+ORDER BY UCASE(CONCAT(em.first_name, ' ', em.last_name)), hs.START_DATE;
