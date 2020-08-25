bfbfe5982714c1d335ab7ebbf8035d8f051df092
Tiago-ers <trsilva284@gmail.com>
2020-08-25 20:53:13 -0300

    desafio5

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT job_title AS 'Cargo', (max_salary - min_salary) AS 'Variação Salarial', 
+SELECT job_title AS 'Cargo', (max_salary - min_salary) AS 'Variação salarial', 
   ROUND(AVG(min_salary/12),2) AS 'Média mínima mensal',
   ROUND(AVG(max_salary/12),2) AS 'Média máxima mensal'
 FROM jobs 
