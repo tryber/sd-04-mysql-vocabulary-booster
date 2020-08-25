7190637f97e0738ae5fdf90651863b1bf632ac6d
wistyk <leonardoxw@gmail.com>
2020-08-25 20:17:37 -0300

    nossa, um s maiúsculo me fazendo passar raiva. ranço

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT (SELECT JOB_TITLE FROM hr.jobs
 WHERE hr.jobs.JOB_ID = hr.employees.JOB_ID) AS 'Cargo',
-ROUND(AVG(salary), 2) AS `Média Salarial`,
+ROUND(AVG(salary), 2) AS `Média salarial`,
 CASE
 WHEN ROUND(AVG(salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -9,4 +9,4 @@ ELSE 'CEO'
 END AS "Senioridade"
 FROM hr.employees
 GROUP BY job_ID
-ORDER BY `Média Salarial`, Cargo;
+ORDER BY `Média salarial`, Cargo;
