bc94074519f71627e1223dfaf5894152724ef1be
Tiago-ers <trsilva284@gmail.com>
2020-08-25 20:28:47 -0300

    cc

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT job_title AS 'Cargo',
- CASE WHEN max_salary >=5000 AND max_salary <= 10000  THEN 'Baixo'
-      WHEN max_salary >=10001 AND max_salary <= 20000 THEN 'Médio'
-      WHEN max_salary >=20001 AND max_salary <= 30000 THEN 'Alto'
-      ELSE 'Altíssimo'
- END AS 'Nível'
+  CASE WHEN max_salary >=5000 AND max_salary <= 10000  THEN 'Baixo'
+    WHEN max_salary >=10001 AND max_salary <= 20000 THEN 'Médio'
+    WHEN max_salary >=20001 AND max_salary <= 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
+  END AS 'Nível'
 FROM jobs ORDER BY Cargo
--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT 
- job_title AS 'Cargo', (max_salary - min_salary) AS 'Variação Salarial', 
- ROUND(AVG(min_salary/12),2) AS 'Média mínima mensal',
- ROUND(AVG(max_salary/12),2) AS 'Média máxima mensal'
+   job_title AS 'Cargo', (max_salary - min_salary) AS 'Variação Salarial', 
+   ROUND(AVG(min_salary/12),2) AS 'Média mínima mensal',
+   ROUND(AVG(max_salary/12),2) AS 'Média máxima mensal'
 FROM jobs 
 GROUP BY job_title
 ORDER BY (max_salary - min_salary), Cargo;
