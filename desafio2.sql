0d3b3e4646d201a57d352fb0b1936e730e06e4cc
Diego Campos <diegodiko05@gmail.com>
2020-08-25 16:38:12 -0300

    Desafio 2, 2

--- desafio2.sql
+++ desafio2.sql
@@ -2,7 +2,7 @@ SELECT job_title as Cargo,
       max_salary,
     CASE
         WHEN max_salary <= 10000 THEN 'Baixo'
-        WHEN max_salary <= 20000 THEN 'Medio'
+        WHEN max_salary <= 20000 THEN 'Médio'
         WHEN max_salary <= 30000 THEN 'Alto'
         ELSE 'Altíssimo'
     END AS Nível
