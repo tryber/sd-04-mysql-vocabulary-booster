ea0e9d65dc1866aa77fbd82825a045e292db7fe7
henrique <hbaetaleite@gmail.com>
2020-08-25 18:30:21 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome Completo',
+(job.JOB_TITLE) AS 'Cargo',
+emp.HIRE_DATE AS 'Data de início do cargo',
+dep.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees emp
+INNER JOIN hr.jobs job
+ON job.JOB_ID = emp.JOB_ID
+INNER JOIN hr.departments dep
+ON emp.DEPARTMENT_ID = dep.DEPARTMENT_ID
+ORDER BY CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) DESC, emp.FIRST_NAME;
