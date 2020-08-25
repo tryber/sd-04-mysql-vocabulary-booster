5203c95eaa7b62cae40a4ed629e153cad65495e9
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 17:02:17 -0300

    me recuso a usar gambiarra

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,4 @@
 SELECT C.COUNTRY_NAME País, IF(C.REGION_ID = 1,'incluído','não incluído') 'Status Inclusão' 
 FROM hr.countries C
+INNER JOIN hr.regions R on C.REGION_ID = R.REGION_ID
 ORDER BY C.COUNTRY_NAME;
