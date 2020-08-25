a8576d8c6c80de2d8d61d49e5ebdd4a02805fe51
matheusysd <matheusyurisd@gmail.com>
2020-08-25 16:13:29 -0300

    CC issues really

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 SELECT job_title as Cargo,
   CASE
-	WHEN max_salary > 4999 AND max_salary < 10001 THEN 'Baixo'
+	  WHEN max_salary > 4999 AND max_salary < 10001 THEN 'Baixo'
     WHEN max_salary < 20001 THEN 'Médio'
     WHEN max_salary < 30001 THEN 'Alto'
     WHEN max_salary > 30000 THEN 'Altíssimo'
