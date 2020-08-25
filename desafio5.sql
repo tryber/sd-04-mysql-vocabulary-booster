da0c583748c1df13a1bc65a112dc339c4bb3f356
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-25 16:04:04 -0300

    desafio 5 cc

--- desafio5.sql
+++ desafio5.sql
@@ -12,6 +12,6 @@
 SELECT * FROM hr.jobs;
 
 SELECT job_title AS 'Cargo', (max_salary - min_salary) AS `Variação salarial`,
- ROUND((min_salary DIV 12), 2) AS `Média mínima mensal`, ROUND((max_salary DIV 12), 2) AS `Média máxima mensal`
+  ROUND((min_salary DIV 12), 2) AS `Média mínima mensal`, ROUND((max_salary DIV 12), 2) AS `Média máxima mensal`
 FROM hr.jobs
 ORDER BY `Variação salarial`, 'Cargo';
