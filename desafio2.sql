dd2f5ad978805902a71c1e8c522fe92decff269e
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 14:55:00 -0300

    Fix CC

--- desafio2.sql
+++ desafio2.sql
@@ -1,10 +1,10 @@
 -- DESAFIO 2
-
 SELECT job_title AS Cargo,
-	CASE
-		WHEN max_salary between 5000 and 10000 THEN 'Baixo'
-        WHEN max_salary between 10001 and 20000 THEN 'Médio'
-        WHEN max_salary between 20001 and 30000 THEN 'Alto'
-        ELSE 'Altissímo'
-    END AS Nível
-FROM hr.jobs ORDER BY job_title;
+CASE
+WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+ELSE 'Altissímo'
+END AS Nível
+FROM hr.jobs
+ORDER BY job_title;
