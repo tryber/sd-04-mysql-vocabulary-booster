53450b9628b4ee9dfa76b9faaf81d1aff28ee5ba
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 19:43:21 -0300

    Fix desafio 11? Será que vai agora?

--- desafio11.sql
+++ desafio11.sql
@@ -7,7 +7,7 @@
 
 -- Try Another Way
 
-SELECT A.contactName AS `Nome`, A.country AS `País`, COUNT(A.contactName) AS `Número de compatriotas`
+SELECT A.contactName AS `Nome`, A.country AS `País`, COUNT(B.contactName) AS `Número de compatriotas`
 FROM w3schools.customers AS A, w3schools.customers AS B
 WHERE A.Country = B.Country AND A.contactName <> B.contactName
 GROUP BY `Nome` ORDER BY `Nome`;
