bfdb8acbf31585da3ccd9ce50538dcbda1bc3142
Joao Soutto <joaosoutto@hotmail.com>
2020-08-25 18:03:21 -0300

    valeu yoshi olhos de lince

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,3 @@
-SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'Incluído', 'Não incluído') AS 'Status Inclusão' FROM hr.countries
-ORDER BY País;
+SELECT COUNTRY_NAME AS `País`, IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries
+ORDER BY `País`;
--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,4 @@ FROM w3schools.customers AS c
 INNER JOIN w3schools.orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido` DESC;
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
