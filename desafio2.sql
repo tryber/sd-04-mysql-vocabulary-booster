407bff5a960f854436797737b553ae42db5cecf3
Thiago Tavares <thiagooo15@gmail.com>
2020-08-25 17:43:21 -0300

    need a break

---
+++ desafio2.sql
@@ -0,0 +1,18 @@
+-- Desafio 2
+-- Com base em todos os cargos presentes, queremos categorizar o nível de remuneração de cada cargo. Utilizando o banco hr como referência, seu relatório deve possuir as seguintes colunas:
+-- A primeira coluna deve possuir o alias "Cargo" e exibir o nome do cargo.
+-- A segunda coluna deve possuir o alias "Nível" e exibir o nível de remuneração do cargo com base no seu salário máximo, categorizando os níveis de remuneração da seguinte forma:
+-- Salário máximo entre 5000 e 10000 -> Baixo
+-- Salário máximo entre 10001 e 20000 -> Médio
+-- Salário máximo entre 20001 e 30000 -> Alto
+-- Salário máximo acima de 30000 -> Altíssimo
+-- Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética.
+SELECT JOB_TITLE AS Cargo,
+CASE 
+	WHEN MAX_SALARY <= 10000 THEN 'Baixo'
+    WHEN MAX_SALARY >= 10001 AND MAX_SALARY <= 20000 THEN 'Médio'
+    WHEN MAX_SALARY >= 20001 AND MAX_SALARY <= 30000 THEN 'Alto'
+    WHEN MAX_SALARY > 30000 THEN 'Altissimo'
+END AS Nível
+FROM hr.jobs
+ORDER BY Cargo;
