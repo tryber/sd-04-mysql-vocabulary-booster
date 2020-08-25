48559533f73be120c362bd3bd232dd9886939874
matheusysd <matheusyurisd@gmail.com>
2020-08-25 13:02:10 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT job_title as Cargo,
+  CASE
+	WHEN max_salary > 4999 AND max_salary < 10001 THEN 'Baixo'
+    WHEN max_salary < 20001 THEN 'Médio'
+    WHEN max_salary < 30001 THEN 'Alto'
+    WHEN max_salary > 30000 THEN 'Altíssimo'
+END AS Nível
+FROM hr.jobs
+ORDER BY job_title ASC;
