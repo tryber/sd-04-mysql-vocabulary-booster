d947eb6bd4f706a0fe9ce2d926f465a3c552122d
Joao Soutto <joaosoutto@hotmail.com>
2020-08-25 17:02:07 -0300

    ate o 8

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,2 @@
-teste
+SELECT COUNTRY_NAME AS País, IF(REGION_ID = 1, 'Incluído', 'Não incluído') AS 'Status Inclusão' FROM hr.countries
+ORDER BY País;
---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome Completo',
+j.JOB_TITLE AS 'Cargo',
+jh.START_DATE AS `Data de início do cargo`,
+dp.DEPARTMENT_NAME AS 'Departamento'
+
+FROM hr.job_history AS jh
+INNER JOIN hr.jobs AS j 
+	ON jh.JOB_ID = j.JOB_ID
+INNER JOIN hr.employees AS e 
+	ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+INNER JOIN hr.departments AS dp 
+	ON jh.DEPARTMENT_ID = dp.DEPARTMENT_ID
+
+ORDER BY `Nome Completo` DESC, `Cargo`;
