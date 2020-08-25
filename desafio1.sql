423f7227798f9344355a1b32d4824929b0f7c253
djonata-dias <diasdjonata@gmail.com>
2020-08-25 17:57:25 -0300

    Ajuste do desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,4 @@
-SELECT c.COUNTRY_NAME AS 'País', IF(r.REGION_ID = 1,'Incluído','Não incluido') AS 'Status Inclusão' FROM hr.countries AS c
+SELECT c.COUNTRY_NAME AS 'País', IF(r.REGION_ID = 1,'incluído','não incluído') AS 'Status Inclusão' FROM hr.countries AS c
 INNER JOIN hr.regions AS r
-WHERE c.REGION_ID = r.REGION_ID;
+WHERE c.REGION_ID = r.REGION_ID
+ORDER BY c.COUNTRY_NAME;
