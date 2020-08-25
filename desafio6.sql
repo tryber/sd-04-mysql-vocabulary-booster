6ba71151f85c0a0408dc206943eb4205b19788a0
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-25 18:13:41 -0300

    6

---
+++ desafio6.sql
@@ -0,0 +1,13 @@
+SELECT
+  CONCAT(e.first_name, ' ', e.last_name) AS 'Nome Completo',
+  j.job_title AS Cargo,
+  DATE(e.hire_date) AS 'Data de início do cargo',
+  department_name AS Departamento
+FROM
+  hr.job_history AS h
+  INNER JOIN hr.employees AS e ON (e.employee_id = h.employee_id)
+  INNER JOIN hr.jobs AS j ON (j.job_id = h.job_id)
+  INNER JOIN hr.departments AS d ON (d.department_id = h.department_id)
+ORDER BY
+  CONCAT(e.first_name, ' ', e.last_name) DESC,
+  Cargo;
