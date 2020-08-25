dfbdffe4861f8f9de0e027ca142e8f6aded907b2
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-25 15:16:13 -0300

    Fix desafio 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT job_title AS Cargo,
 MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
-ROUND(MIN_SALARY DIV 12, 2) AS 'Média mínima mensal',
-ROUND(MAX_SALARY DIV 12, 2) AS 'Média máxima mensal'
+ROUND(MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+ROUND(MAX_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs as job
-ORDER BY (max_salary - min_salary), Cargo;
+ORDER BY `Variação Salarial`, Cargo;
