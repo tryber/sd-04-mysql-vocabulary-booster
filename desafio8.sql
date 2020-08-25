e9793abd73ab8f8337e42e74d62e3f93f52413c4
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 19:37:11 -0300

    Fix Desafio 8 - Change customerName > contactName

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,6 @@
 -- DESAFIO 8
 
-SELECT C.customerName AS 'Nome de contato',
+SELECT C.contactName AS 'Nome de contato',
 S.shipperName AS 'Empresa que fez o envio',
 O.orderDate AS 'Data do pedido'
 FROM w3schools.customers AS C
@@ -9,4 +9,4 @@ ON C.customerId = O.CustomerId
 INNER JOIN w3schools.shippers AS S
 ON O.shipperId = S.shipperId
 WHERE S.shipperName IN ('Speedy Express', 'United Package')
-ORDER BY C.customerName, S.shipperName, O.orderDate;
+ORDER BY C.contactName, S.shipperName, O.orderDate;
