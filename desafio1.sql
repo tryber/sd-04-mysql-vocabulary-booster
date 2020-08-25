5af0fd5dc6a9cc47e6d0ce96c2917a8f777812c2
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 16:35:55 -0300

    req 1 CC 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,3 @@
-USE HR;
-SELECT C.COUNTRY_NAME País, 
-IF(C.REGION_ID = 1,'incluído','não incluído') 'Status Inclusão' 
-FROM countries C
+SELECT C.COUNTRY_NAME País, IF(C.REGION_ID = 1,'incluído','não incluído') 'Status Inclusão' 
+FROM hr.countries C
 ORDER BY C.COUNTRY_NAME;
