5fecda4ab6acb88d0b1fca16608d57559a939bbd
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 15:48:08 -0300

    Fix Desafio 6 - using job_history

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,14 @@
 
 SELECT CONCAT(E.first_name, ' ', E.last_name) AS 'Nome completo',
 J.job_title AS Cargo,
-E.hire_date AS 'Data de início do cargo',
+JH.start_date AS 'Data de início do cargo',
 D.department_name AS Departamento
-FROM hr.employees AS E
+FROM hr.employees as E
+INNER JOIN hr.job_history AS JH
+ON E.employee_id = JH.employee_id
 INNER JOIN hr.jobs AS J
-ON E.job_id = J.job_id
+ON JH.job_id = J.job_id
 INNER JOIN hr.departments AS D
-ON E.department_id = D.department_id
-ORDER BY CONCAT(E.first_name, ' ', E.last_name) DESC, Departamento ASC;
+ON JH.department_id = D.department_id
+ORDER BY CONCAT(E.first_name, ' ', E.last_name) DESC, J.job_title ASC;
+
