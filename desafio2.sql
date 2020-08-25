1386d8d24ba2d60701884f09b6918497599c5f51
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-25 17:33:14 -0300

    desafio 02 - 04

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 SELECT
   JOB_TITLE AS Cargo,
   CASE
-    WHEN MAX_SALARY < 10000 THEN 'Baixo'
+    WHEN MAX_SALARY < 10001 THEN 'Baixo'
     WHEN MAX_SALARY < 20001 THEN 'Médio'
     WHEN MAX_SALARY < 30001 THEN 'Alto'
     ELSE 'Altíssimo'
