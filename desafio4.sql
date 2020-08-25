3e995a8a82337246282e9de2fdff3f014e95bffa
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-25 19:36:49 -0300

    cc desafio 4 e 5

--- desafio4.sql
+++ desafio4.sql
@@ -2,15 +2,15 @@
 -- Em caso de empate na média, os resultados devem ser ordenados pelo nome do cargo em ordem alfabética. 
 SELECT * FROM hr.jobs;
 SELECT 
-J.JOB_TITLE AS Cargo, ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
+J.JOB_TITLE AS `Cargo`, ROUND(AVG(E.SALARY), 2) AS `Média salarial`,
 CASE 
 WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
 WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
 WHEN ROUND(AVG(E.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
 ELSE 'CEO'
-END AS `Senioridade`
+END AS 'Senioridade'
 FROM
 hr.jobs AS J
 INNER JOIN
 hr.employees AS E ON J.JOB_ID = E.JOB_ID
-GROUP BY J.JOB_TITLE ORDER BY `Média salarial`, Cargo;
+GROUP BY J.JOB_TITLE ORDER BY `Média salarial`, `Cargo`;
