a405047ffbbf49b6f81ffa5c4287ea2c2430fcee
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-25 17:33:23 -0300

    aa

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,7 @@
 --  select * from hr.employees;
 --  select * from hr.job_history;
-SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`, history.START_DATE AS `Data de início` FROM hr.employees AS employees
+SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`, history.START_DATE AS `Data de início`, employees.SALARY AS `Salário`
+FROM hr.employees AS employees
 INNER JOIN hr.job_history AS history on employees.EMPLOYEE_ID = history.EMPLOYEE_ID
 WHERE MONTH(history.START_DATE) BETWEEN '01' AND '03'
 ORDER BY `Nome completo`, `Data de início`;
