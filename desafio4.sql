aa34ff136100945af28485b29a9975149b97d765
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-25 14:42:28 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT job.job_title AS Cargo,
+ROUND(AVG(em.salary), 2) AS 'Média salarial',
+	CASE
+		WHEN ROUND(AVG(em.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(em.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(em.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+END AS Senioridade
+FROM hr.jobs as job
+INNER JOIN hr.employees as em
+ON job.job_id = em.job_id
+GROUP BY job_title
+ORDER BY ROUND(AVG(em.salary), 2), Cargo;
