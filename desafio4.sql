79adcc8695fffb48e566cddca06ac5fe48a4509f
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-25 17:02:59 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,17 @@
+SELECT
+  j.job_title AS Cargo,
+  ROUND(AVG(e.salary), 2) AS 'Média salarial',
+  CASE
+    WHEN ROUND(AVG(e.salary), 2) > 10500 THEN 'CEO'
+    WHEN ROUND(AVG(e.salary), 2) > 7500 THEN 'Sênior'
+    WHEN ROUND(AVG(e.salary), 2) > 5800 THEN 'Pleno'
+    WHEN ROUND(AVG(e.salary), 2) >= 2000 THEN 'Júnior'
+  END AS Senioridade
+FROM
+  hr.jobs AS j
+  INNER JOIN hr.employees AS e ON (j.job_id = e.job_id)
+GROUP BY
+  Cargo
+ORDER BY
+  ROUND(AVG(e.salary), 2),
+  Cargo;
