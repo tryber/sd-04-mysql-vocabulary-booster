501c454eaf333aabcd0c10cf0f018ba868845036
AliceAAtalla <aliceatalla36@gmail.com>
2020-08-25 20:55:30 -0300

    desafio 18 tentativa 2

--- desafio18.sql
+++ desafio18.sql
@@ -4,7 +4,7 @@ DELIMTER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(IN mes INT, IN ano INT)
 RETURNS INT READS SQL DATA
 BEGIN
-DECLARE qnt_pessoas;
+DECLARE qnt_pessoas INT;
 SELECT COUNT(*) FROM hr.employees
 WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano INTO qnt_pessoas;
 RETURN qnt_pessoas;
