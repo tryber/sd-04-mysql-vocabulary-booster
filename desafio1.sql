f4f825939a53da89a9644e90be3c5d90365d86f9
samueap <samueap@gmail.com>
2020-08-25 16:04:29 -0300

    initial commit

---
+++ desafio1.sql
@@ -0,0 +1,10 @@
+-- Requisito 1
+
+-- Seleciona coluna country_name e nomeia para País
+-- (IF) verifica se region_id = 1 => Europe, se true=incluído, false=não incluído
+-- renomeia coluna region_id por Status Inclusão e coloca resultado booleano
+-- Verifica-se na tabela hr.countries e ordena por países ascedentes
+SELECT country_name AS País,
+	IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries 
+ORDER BY País;
