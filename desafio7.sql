d2d6c3a9c368e8d68555589c962a5c606067fe85
Diego Campos <diegodiko05@gmail.com>
2020-08-25 19:11:26 -0300

    Formatacao dos arquivos

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,12 @@
-SELECT upper(concat(e.first_name, ' ', e.last_name)) as 'Nome completo',
-h.start_date as 'Data de início do cargo', e.salary as 'Salario'
-FROM hr.employees as e
-INNER JOIN hr.job_history as h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
-Where month(h.start_date) < 4
-ORDER BY concat(e.first_name, ' ', e.last_name), h.start_date;
+SELECT
+  upper(concat(e.first_name, ' ', e.last_name)) as 'Nome completo',
+  h.start_date as 'Data de início do cargo',
+  e.salary as 'Salario'
+FROM
+  hr.employees as e
+  INNER JOIN hr.job_history as h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+Where
+  month(h.start_date) < 4
+ORDER BY
+  concat(e.first_name, ' ', e.last_name),
+  h.start_date;
