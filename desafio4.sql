51b78f0628f2a47b1bf607cca5b1fb0a59294288
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-25 17:18:48 -0300

    CC 01

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
 select j.job_title as Cargo, round(avg(e.salary), 2) as 'Média salarial',
-	case
-		when avg(e.salary) >= 2000 and avg(e.salary) <= 5800 then 'Júnior'
-        when avg(e.salary) >= 5801 and avg(e.salary) <= 7500 then 'Pleno'
-        when avg(e.salary) >= 7501 and avg(e.salary) <= 10500 then 'Sênior'
-        else 'CEO'
-	end as 'Senioridade'
+case
+when avg(e.salary) >= 2000 and avg(e.salary) <= 5800 then 'Júnior'
+when avg(e.salary) >= 5801 and avg(e.salary) <= 7500 then 'Pleno'
+when avg(e.salary) >= 7501 and avg(e.salary) <= 10500 then 'Sênior'
+else 'CEO'
+end as 'Senioridade'
 from jobs j, employees e
 where j.job_id = e.job_id
 group by j.job_title
