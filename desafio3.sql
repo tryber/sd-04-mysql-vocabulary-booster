9ff82046f637711149de9c3487841939183cd7b5
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-25 16:26:13 -0300

    dasafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -12,4 +12,4 @@ SELECT
     MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
 FROM
     hr.jobs
-ORDER BY `Diferença entre salários máximo e mínimo`, `cargo`;
+ORDER BY `Diferença entre salários máximo e mínimo`, `Cargo`;
