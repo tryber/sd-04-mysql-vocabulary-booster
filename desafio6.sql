2c04be2870d9684fba4ea4619336caf3f2ef0307
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-25 18:14:43 -0300

    Correção desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,10 @@
-SELECT * FROM hr.job_history;
-SELECT UCASE(CONCAT(A.FIRST_NAME,' ',A.LAST_NAME)) AS 'Nome Completo', B.START_DATE as 'Data de início',
-A.SALARY AS 'Salário'
+SELECT UCASE(CONCAT(A.FIRST_NAME,' ',A.LAST_NAME)) AS 'Nome Completo',
+A.JOB_ID AS 'Cargo',
+B.START_DATE as 'Data de início',
+C.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees as A
 INNER JOIN hr.job_history AS B
 ON A.EMPLOYEE_ID = B.EMPLOYEE_ID
--- WHERE MONTH(B.START_DATE IN (1,2,3))
-;
+LEFT JOIN hr.departments AS C
+ON A.DEPARTMENT_ID = C.DEPARTMENT_ID
+ORDER BY 1 DESC, 2;
