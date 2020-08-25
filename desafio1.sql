117bdf120ee3febe0d7e24120dbd58dd1a23e7fb
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-25 16:09:44 -0300

    Desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,5 @@
+SELECT A.COUNTRY_NAME AS 'País',
+IF(B.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS A
+INNER JOIN hr.regions AS B
+ON (A.REGION_ID = B.REGION_ID);
