8a4106f78d716d5817277d21d40d808d0f1d3118
wistyk <leonardoxw@gmail.com>
2020-08-25 18:10:54 -0300

    req4

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
-SELECT job_Title AS 'Cargo', concat(max_salary - min_salary)
+SELECT job_Title AS 'Cargo', max_salary - min_salary
 AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY max_salary - min_salary ASC, Cargo ASC;
+ORDER BY 2, 1;
