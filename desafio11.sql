04a62a69d8fffdc3107fb41f0a5afb059c298b96
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-25 20:19:48 -0300

    untill 11

---
+++ desafio11.sql
@@ -0,0 +1,16 @@
+SELECT
+  c.ContactName AS 'Nome',
+  c.Country AS 'País',
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      w3schools.customers AS subc
+    WHERE
+      subc.Country = c.Country
+      AND subc.ContactName <> c.ContactName
+  ) AS 'Número de compatriotas'
+FROM
+  w3schools.customers AS c
+ORDER BY
+  `Nome`;
