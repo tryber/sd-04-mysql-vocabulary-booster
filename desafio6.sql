0d5621b092e02f2b4f12096caefbc3bcaeacef9a
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-08-25 18:21:38 -0300

    req6 CC

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.first_name, " ", e.last_name) AS 'Nome completo',
-	j.JOB_TITLE AS Cargo,
-  h.START_DATE AS 'Data de início do cargo',
-  d.DEPARTMENT_NAME AS 'Departamento'
+j.JOB_TITLE AS Cargo,
+h.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
 INNER JOIN hr.job_history AS h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
