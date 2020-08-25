6f794d4a8b401b56515c54a4316767678fc426e8
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-25 15:17:47 -0300

    desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -16,3 +16,11 @@
 -- Salário máximo acima de 30000 -> Altíssimo
 
 -- Os resultados devem estar ordenados pelo nome do cargo em ordem alfabética.
+SELECT JOB_TITLE AS `Cargo`,
+CASE
+WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
+WHEN MAX_SALARY BETWEEN 10001 AND 120000 THEN 'Médio'
+WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
+ELSE 'Altíssimo'
+END AS 'Nível'
+FROM hr.jobs ORDER BY `cargo`;
