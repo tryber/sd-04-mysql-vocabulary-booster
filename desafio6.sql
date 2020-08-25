03662c9cc938bfba8838fdfffb903465c857345a
Diego Campos <diegodiko05@gmail.com>
2020-08-25 18:30:35 -0300

    Desafio 6, 3

--- desafio6.sql
+++ desafio6.sql
@@ -3,7 +3,7 @@ j.job_title AS Cargo,
 h.start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
 FROM hr.employees AS e
-INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID 
 INNER JOIN hr.job_history as h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.departments as d ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+INNER JOIN hr.jobs AS j ON j.JOB_ID = h.JOB_ID 
 order by concat(e.first_name, ' ', e.last_name) desc, Cargo;
