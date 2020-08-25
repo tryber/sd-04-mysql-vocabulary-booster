626f5f24a130d6cb07fa0974ae63db85709c2097
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-08-25 18:20:38 -0300

    req6

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(e.first_name, " ", e.last_name) AS 'Nome completo',
+	j.JOB_TITLE AS Cargo,
+  h.START_DATE AS 'Data de início do cargo',
+  d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS e
+INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+INNER JOIN hr.job_history AS h ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = h.DEPARTMENT_ID
+ORDER BY CONCAT(e.first_name, " ", e.last_name) DESC, Cargo;
