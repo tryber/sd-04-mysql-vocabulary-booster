65d6cb9ddc339ba3d70b727fe7c228425d2344e6
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-25 16:14:21 -0300

    Unexpected found found in the end case.

--- desafio4.sql
+++ desafio4.sql
@@ -5,7 +5,7 @@ SELECT
     WHEN ((jobs.MIN_SALARY + jobs.MAX_SALARY) / 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN ((jobs.MIN_SALARY + jobs.MAX_SALARY) / 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
     WHEN ((jobs.MIN_SALARY + jobs.MAX_SALARY) / 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-	  ELSE 'CEO'
+    ELSE 'CEO'
   END AS Senioridade
 FROM hr.jobs AS jobs
 INNER JOIN hr.employees AS emp
