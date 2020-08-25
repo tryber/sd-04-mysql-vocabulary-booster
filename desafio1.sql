6ff5072c4d196957eb82c57ef034e1ea4a5ad769
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-25 15:31:38 -0300

    Codeclimate issues - 1

--- desafio1.sql
+++ desafio1.sql
@@ -7,5 +7,7 @@ Foi decidido que vamos continuar atendendo apenas à região "Europe".
      e deve exibir "incluído" se o país em questão está incluso em "Europe", ou "não incluído", caso contrário.
 Os resultados devem estar ordenados pelo nome do país em ordem alfabética.
 */
-select * from hr.locations;
-
+SELECT COUNTRY_NAME AS 'País',
+  IF(REGION_ID = 1, 'incluído','não incluído') AS 'Status Inclusão'
+  FROM hr.countries
+  ORDER BY COUNTRY_NAME ASC;
