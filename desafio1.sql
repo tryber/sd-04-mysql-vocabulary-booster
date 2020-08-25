2c791a0b15eb5e9a2c2201e022e328d63a8668e7
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-25 16:11:53 -0300

    Ordem alfabética

--- desafio1.sql
+++ desafio1.sql
@@ -2,4 +2,5 @@ SELECT A.COUNTRY_NAME AS 'País',
 IF(B.REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS A
 INNER JOIN hr.regions AS B
-ON (A.REGION_ID = B.REGION_ID);
+ON (A.REGION_ID = B.REGION_ID)
+ORDER BY A.COUNTRY_NAME;
