3334758ff18e8d1acc650a2793cbd1143f416479
Tiago-ers <trsilva284@gmail.com>
2020-08-25 15:15:41 -0300

    desafio1 - correçao nome coluna

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,3 @@
-SELECT country_name AS 'Pais', IF(region_id = '1', 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT country_name AS 'País', IF(region_id = '1', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM countries
 ORDER BY country_name;
