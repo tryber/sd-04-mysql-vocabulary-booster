fe7d702515f3972f2c7095b904fca4c17a37bf2b
Julio Cesar <stininmatic@hotmail.com>
2020-08-25 20:13:58 -0300

    Passos de tartaruga ALTISSIMO

--- desafio2.sql
+++ desafio2.sql
@@ -3,7 +3,7 @@ CASE
 WHEN J.MAX_SALARY between 5000 and 10000 then 'Baixo'
 WHEN J.MAX_SALARY between 10001 and 20000 then 'Médio'
 WHEN J.MAX_SALARY between 20001 and 30000 then 'Alto'
-ELSE 'Altissímo'
+ELSE 'Altíssimo'
 end) Nível
 from hr.jobs J
 order by Cargo;
