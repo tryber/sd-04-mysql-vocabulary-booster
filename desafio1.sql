6637c7025a0ce681840bc12e7bfebef4ddc60302
Julio Cesar <stininmatic@hotmail.com>
2020-08-25 17:11:41 -0300

    Oh s here we go again

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
 select c.country_name as 'País',
-if(c.region_id = 1, 'incluído', 'não incluído') as 'Status Inclusao'
+if(c.region_id = 1, 'incluído', 'não incluído') as 'Status Inclusão'
 from hr.countries c
+inner join hr.regions r on r.REGION_ID = c.REGION_ID
 order by País;
