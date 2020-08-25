aa367af03a2e7d974242b40a5758ff36ef9ac33e
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 19:27:16 -0300

    Try fix desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,13 @@
 -- DESAFIO 11
 
-SELECT A.contactName AS `Nome`, A.country AS `País`, COUNT(A.contactName) - 1 AS `Número de compatriotas` -- Gambiarra
+-- SELECT A.contactName AS `Nome`, A.country AS `País`, (COUNT(A.contactName) - 1) AS `Número de compatriotas` -- Gambiarra
+-- FROM w3schools.customers AS A, w3schools.customers AS B
+-- WHERE A.Country = B.Country
+-- GROUP BY `Nome` ORDER BY A.ContactName;
+
+-- Try Another Way
+
+SELECT A.contactName AS `Nome`, A.country AS `País`, COUNT(A.contactName) AS `Número de compatriotas`
 FROM w3schools.customers AS A, w3schools.customers AS B
-WHERE A.Country = B.Country
-GROUP BY A.ContactName ORDER BY A.ContactName;
+WHERE A.Country = B.Country AND A.contactName <> B.contactName
+GROUP BY `Nome` ORDER BY `Nome`;
