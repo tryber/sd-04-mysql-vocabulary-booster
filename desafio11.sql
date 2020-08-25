55be6c1175f885e1aef1544896adefda12b37df2
leandromarten1 <leandromarten1@gmail.com>
2020-08-25 19:53:44 -0300

    Ajuda do JUNIO agora VAI!

--- desafio11.sql
+++ desafio11.sql
@@ -6,8 +6,10 @@
 -- GROUP BY `Nome` ORDER BY A.ContactName;
 
 -- Try Another Way
-
+-- Refatorado depois de fazer o Desafio 12 onde usei o <> para retirar o nome "duplicado"!
+-- Alterado o GROUP BY com a Ajuda no JUNIO > https://github.com/tryber/sd-04-mysql-vocabulary-booster/pull/11/files
 SELECT A.contactName AS `Nome`, A.country AS `País`, COUNT(B.contactName) AS `Número de compatriotas`
 FROM w3schools.customers AS A, w3schools.customers AS B
 WHERE A.Country = B.Country AND A.contactName <> B.contactName
-GROUP BY `Nome` ORDER BY `Nome`;
+GROUP BY A.CustomerId ORDER BY `Nome`;
+
