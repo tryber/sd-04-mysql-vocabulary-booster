296446dd5589e5754cd92ceab87606a1fe4baaa4
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 20:50:24 -0300

    req 7 corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,3 @@
-select * from employees;
-
 SELECT UPPER(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) 'Nome completo', J.JOB_TITLE Cargo, JH.START_DATE 'Data de início', E.SALARY Salário
 FROM hr.employees E
 INNER JOIN hr.job_history JH on JH.EMPLOYEE_ID = E.EMPLOYEE_ID
