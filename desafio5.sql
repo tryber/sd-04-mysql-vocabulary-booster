90279dcb1bcf6dff0026ef8c9d42fc4ad6756bfd
Pedro <pedrotpoliveira@gmail.com>
2020-08-25 23:30:04 -0300

    Evaluator Fix - Desafios 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,7 @@
-SELECT jb.JOB_TITLE AS `Cargo`, 
-(jb.MAX_SALARY-jb.MIN_SALARY) AS `Variação Salarial`, 
-ROUND((jb.MIN_SALARY/12),2) AS `Média mínima mensal`,
-ROUND((jb.MAX_SALARY/12),2) AS `Média máxima mensal`
-FROM hr.employees as em
-INNER JOIN hr.jobs as jb
-ON em.JOB_ID=jb.JOB_ID
-GROUP BY jb.JOB_TITLE
+SELECT JOB_TITLE AS `Cargo`, 
+(MAX_SALARY-MIN_SALARY) AS `Variação Salarial`, 
+ROUND((MIN_SALARY/12),2) AS `Média mínima mensal`,
+ROUND((MAX_SALARY/12),2) AS `Média máxima mensal`
+FROM hr.jobs
+GROUP BY JOB_TITLE
 ORDER BY `Variação Salarial`, `Cargo`;
