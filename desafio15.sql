4e152e11fdf8c523014df89fb0fe2abf926c2375
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-26 12:18:05 -0300

    16

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+CREATE PROCEDURE buscar_media_por_cargo (IN cargo VARCHAR(35)) BEGIN
+SELECT
+  ROUND(AVG(e.salary), 2) AS 'Média salarial'
+FROM
+  hr.employees AS e
+  INNER JOIN hr.jobs AS j ON (e.job_id = j.job_id)
+WHERE
+  cargo = j.job_title;
+END $$
+
+DELIMITER ;
+
+call buscar_media_por_cargo('Programmer');
