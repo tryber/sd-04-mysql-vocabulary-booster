35c5a365f028997753f4d014821c9e61a1d2f5af
djonata-dias <diasdjonata@gmail.com>
2020-08-25 22:21:44 -0300

    Desafio 8 teste 3

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 SELECT
+    c.ContactName AS 'Nome de contato',
     s.ShipperName AS 'Empresa que fez o envio',
-    o.OrderDate AS 'Data do pedido',
-    c.ContactName AS 'Nome de contato'
+    o.OrderDate AS 'Data do pedido'
 FROM
     w3schools.orders AS o
     INNER JOIN w3schools.customers AS c ON c.CustomerID = o.CustomerID
-    INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+    INNER JOIN w3schools.shippers AS s ON s.ShipperID = o.ShipperID
 ORDER BY
     c.ContactName ASC,
     s.ShipperName ASC,
