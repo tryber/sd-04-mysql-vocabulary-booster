bb32d1e712fc9d64ac88f54d593c69e5ea30cbe7
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-26 15:52:05 -0300

    desafio13

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+-- select * from w3schools.order_details;
+-- select * from w3schools.products;
+SELECT
+  p.ProductName AS Produto,
+  p.Price AS Preço
+FROM w3schools.order_details AS od
+INNER JOIN w3schools.products AS p ON od.ProductID = p.ProductID
+WHERE od.Quantity > 80
+ORDER BY Produto;
