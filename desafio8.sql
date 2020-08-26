ef99715da23df1c146017fdf7d14fb1354920c15
djonata-dias <diasdjonata@gmail.com>
2020-08-25 21:55:24 -0300

    Desafio 7 teste 3

--- desafio8.sql
+++ desafio8.sql
@@ -6,8 +6,6 @@ FROM
     hr.employees AS e
     INNER JOIN hr.job_history h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
     INNER JOIN hr.jobs j ON h.JOB_ID = j.JOB_ID
-WHERE
-    MONTH(h.START_DATE) IN (1, 2, 3)
 ORDER BY
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ASC,
     h.START_DATE ASC;
