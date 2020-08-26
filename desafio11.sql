ed35c4874a5cf623f9fe2d73b6cb05d74c50e80d
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-26 00:02:14 -0300

    será possível???

---
+++ desafio11.sql
@@ -0,0 +1,9 @@
+SELECT
+t1.ContactName AS `Nome`,
+t1.Country AS `País`,
+COUNT(t2.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS t1,
+w3schools.customers AS t2
+WHERE t1.Country = t2.Country
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;
---
+++ desafio12.sql
@@ -0,0 +1,10 @@
+SELECT
+CONCAT(funcA.FIRST_NAME, ' ', funcA.LAST_NAME) AS `Nome completo funcionário 1`,
+funcA.SALARY AS `Salário funcionário 1`,
+funcA.PHONE_NUMBER AS `Telefone funcionário 1`,
+CONCAT(funcB.FIRST_NAME, ' ', funcB.LAST_NAME) AS `Nome completo funcionário 2`,
+funcB.SALARY AS `Salário funcionário 2`,
+funcB.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees AS funcA, hr.employees AS funcB
+WHERE funcA.JOB_ID = funcB.JOB_ID
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
