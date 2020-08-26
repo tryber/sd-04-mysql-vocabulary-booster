63c280885cf2a18b74e80f58ee68f311d87ce673
djonata-dias <diasdjonata@gmail.com>
2020-08-25 21:25:12 -0300

    Desafio 6 teste 1

---
+++ desafio6.sql
@@ -0,0 +1,10 @@
+SELECT 
+CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) AS 'Nome completo',e.EMPLOYEE_ID,
+ j.JOB_TITLE AS 'Cargo',
+ h.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
+FROM hr.employees AS e
+INNER JOIN hr.jobs j ON e.JOB_ID = j.JOB_ID
+LEFT JOIN hr.job_history h ON j.JOB_ID = h.JOB_ID
+INNER JOIN hr.departments d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
+ORDER BY CONCAT(e.FIRST_NAME,' ',e.LAST_NAME) DESC,  j.JOB_TITLE ASC;
