8ab595a68c0d01707803adeda0d8f74e7f5348b2
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-26 17:52:30 -0300

    cc

--- desafio6.sql
+++ desafio6.sql
@@ -2,4 +2,3 @@ select concat(e.first_name," ",e.last_name) as 'Nome completo', j.job_title, jh.
 from jobs j, employees e, departments d, job_history jh
 where e.JOB_ID = j.JOB_ID and e.DEPARTMENT_ID = d.DEPARTMENT_ID and e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 order by 1 desc, 2;
-
