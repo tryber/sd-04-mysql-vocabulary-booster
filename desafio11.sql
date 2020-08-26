c85e082b6ec08e126307cab9c1b8d540a5b1b767
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-26 00:10:51 -0300

    agora vai

--- desafio11.sql
+++ desafio11.sql
@@ -4,6 +4,6 @@ t1.Country AS `País`,
 COUNT(t2.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS t1,
 w3schools.customers AS t2
-WHERE t1.Country = t2.Country
+WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID
 GROUP BY `Nome`, `País`
 ORDER BY `Nome`;
