009391260abdaf9b1ec5d647d015340805946d8f
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-25 21:36:00 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -16,8 +16,8 @@
 SELECT 
     CONCAT(E.FIRST_NAME, '  ', E.LAST_NAME) AS `Nome completo`,
     J.JOB_TITLE AS `Cargo`,
-    H.START_DATE AS 'Data de início do cargo',
-    D.DEPARTMENT_NAME AS 'Departamento'
+    H.START_DATE AS `Data de início do cargo`,
+    D.DEPARTMENT_NAME AS `Departamento`
 FROM
     hr.job_history AS H
         INNER JOIN
@@ -27,4 +27,3 @@ FROM
         INNER JOIN
     hr.departments AS D ON D.DEPARTMENT_ID = H.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , `Cargo` ASC;
-
