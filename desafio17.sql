5346a8a9dc790d58fe1f7cd6e2ad36ba4d710bda
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-26 18:45:02 -0300

    Resultados arredondados

--- desafio17.sql
+++ desafio17.sql
@@ -2,6 +2,6 @@ SELECT
   CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
   DATE_FORMAT(jh.START_DATE, "%d/%m/%Y") AS `Data de início`,
   DATE_FORMAT(jh.END_DATE, "%d/%m/%Y") AS `Data de rescisão` ,
-  DATEDIFF(jh.END_DATE, jh.START_DATE) DIV 365 AS `Anos trabalhados` from hr.job_history as jh
+  ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS `Anos trabalhados` from hr.job_history as jh
 INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY `Nome completo`, `Anos trabalhados`;
