b8f3febb936975ccb069943e282d353a231a9fff
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-26 00:28:00 -0300

    desafio10.sql

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 --  select * from hr.employees;
 --  select * from hr.job_history;
-SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`, history.START_DATE AS `Data de início`, employees.SALARY AS `Salário`
+SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`, h.START_DATE AS `Data de início`, employees.SALARY AS `Salário`
 FROM hr.employees AS employees
-INNER JOIN hr.job_history AS history on employees.EMPLOYEE_ID = history.EMPLOYEE_ID
-WHERE MONTH(history.START_DATE) BETWEEN '01' AND '03'
+INNER JOIN hr.job_history AS h on employees.EMPLOYEE_ID = h.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
 ORDER BY `Nome completo`, `Data de início`;
--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(e.FirstName, ' ', e.LastName) AS `Nome completo`, COUNT(e.EmployeeID) AS `Total de pedidos`
-FROM w3schools.employees AS e
-INNER JOIN w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+FROM w3schools.orders AS o
+INNER JOIN w3schools.employees AS e ON o.EmployeeID = e.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`; 
