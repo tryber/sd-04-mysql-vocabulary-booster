d48d2ca475e0f7bc397e43fa3f9d1b8e25a83ed0
henrique <hbaetaleite@gmail.com>
2020-08-26 12:57:54 -0300

    desafio 6, la vamos nos

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,13 @@
-SELECT jobs.job_title AS 'Cargo',
-  ROUND(AVG(employees.salary), 2) AS `Média salarial`,
+SELECT jobs.job_title AS `Cargo`,
+  ROUND(AVG(em.salary), 2) AS `Média salarial`,
   CASE
-    WHEN ROUND(AVG(employees.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
-    WHEN ROUND(AVG(employees.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
-    WHEN ROUND(AVG(employees.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
-    WHEN ROUND(AVG(employees.salary), 2) > 10500 THEN 'CEO'
+    WHEN ROUND(AVG(em.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(em.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(em.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(em.salary), 2) > 10500 THEN 'CEO'
   END AS 'Senioridade'
 FROM hr.jobs AS jobs
-  JOIN hr.employees AS employees 
-  ON jobs.job_id = employees.job_id
+  JOIN hr.employees AS em 
+  ON jobs.job_id = em.job_id
 GROUP BY jobs.job_title
-ORDER BY `Média salarial`, 'Cargo';
+ORDER BY `Média salarial`, `Cargo`;
--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,14 @@
-SELECT CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome Completo',
-job.JOB_TITLE AS 'Cargo',
-his.START_DATE AS 'Data de início do cargo',
-dep.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.job_history AS his
-INNER JOIN hr.jobs AS job
-ON his.JOB_ID = job.JOB_ID
-INNER JOIN hr.employees AS emp
-ON emp.EMPLOYEE_ID = his.EMPLOYEE_ID
-INNER JOIN hr.departments AS dep
-ON his.DEPARTMENT_ID = dep.DEPARTMENT_ID
-ORDER BY 'Nome Completo' DESC, 'Cargo';
+SELECT 
+    CONCAT(emp.first_name, ' ', emp.last_name) AS `Nome completo`,
+    jobs.job_title AS `Cargo`,
+    history.start_date AS 'Data de início do cargo',
+    departments.department_name AS 'Departamento'
+FROM
+    hr.job_history AS history
+        INNER JOIN
+    hr.jobs AS jobs ON history.job_id = jobs.job_id
+        INNER JOIN
+    hr.employees AS emp ON emp.employee_id = history.employee_id
+        INNER JOIN
+    hr.departments AS departments ON history.department_id = departments.department_id
+ORDER BY `Nome completo` DESC , `Cargo`;
