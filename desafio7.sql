0d7b62861fb196610e0268ea323432e802c25c40
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-25 22:40:31 -0300

    nada ta fazendo sentido nesses resultados

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT
+UCASE(CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME)) AS 'Nome completo',
+t1.START_DATE AS 'Data de início',
+t2.salary AS 'Salário'
+FROM hr.job_history AS t1
+INNER JOIN hr.employees t2 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
+WHERE MONTH(t1.START_DATE ) IN(1, 2, 3)
+ORDER BY CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME), t1.START_DATE;
