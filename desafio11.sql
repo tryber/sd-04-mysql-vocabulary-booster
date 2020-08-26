b7758e987aea4677cc2a76554cb722791462c9f1
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-26 14:51:46 -0300

    vai de self join mesmo

--- desafio11.sql
+++ desafio11.sql
@@ -1,17 +1,27 @@
-DELIMITER $$CREATE FUNCTION calcCompatriots(varCountry VARCHAR(100))
-RETURNS INT READS SQL DATA
-BEGIN
-  DECLARE compatriots INT;
-  SELECT COUNT(*) - 1 FROM w3schools.customers WHERE w3schools.customers.Country = varCountry
-  INTO compatriots;
-  RETURN compatriots;
-END
-$$
-DELIMITER ;
-SELECT
-  ContactName AS Nome,
-  Country AS `País`,
-  calcCompatriots(Country) AS `Número de compatriotas`
-FROM w3schools.customers
-WHERE calcCompatriots(Country) > 0
+-- USE w3schools;
+-- DELIMITER $$
+-- CREATE FUNCTION calcCompatriots(varCountry VARCHAR(100))
+-- RETURNS INT READS SQL DATA
+-- BEGIN
+--   DECLARE compatriots INT;
+--   SELECT COUNT(*) - 1 FROM w3schools.customers
+--   WHERE w3schools.customers.Country = varCountry
+--   INTO compatriots;
+--   RETURN compatriots;
+-- END$$
+-- DELIMITER ;
+-- SELECT
+--   ContactName AS Nome,
+--   Country AS `País`,
+--   calcCompatriots(Country) AS `Número de compatriotas`
+-- FROM w3schools.customers
+-- WHERE calcCompatriots(Country) > 0
+-- ORDER BY Nome;
+SELECT 
+  c1.ContactName AS Nome,
+  c1.Country AS País,
+  COUNT(c2.CustomerID) AS `Número de compatriotas`
+FROM w3schools.customers AS c1, w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
+GROUP BY c1.CustomerID
 ORDER BY Nome;
--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,10 @@
 --  select * from hr.employees;
 --  select * from hr.job_history;
-SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`, h.START_DATE AS `Data de início`, employees.SALARY AS `Salário`
+SELECT
+  UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
+  h.START_DATE AS `Data de início`,
+  employees.SALARY AS `Salário`
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS h on employees.EMPLOYEE_ID = h.EMPLOYEE_ID
-WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
-ORDER BY `Nome completo`, `Data de início`;
+  WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
+  ORDER BY `Nome completo`, `Data de início`;
