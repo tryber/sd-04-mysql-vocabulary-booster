2637304cda23b9b34405e3e038ecce1bf64eee1e
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-08-26 17:45:29 -0300

    desafios 3 e 4

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+SELECT 
+    job_title AS Cargo,
+    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+FROM
+    jobs
+ORDER BY (MAX_SALARY - MIN_SALARY) , Cargo;
---
+++ desafio4.sql
@@ -0,0 +1,19 @@
+SELECT 
+    (SELECT 
+            job_title
+        FROM
+            jobs j
+        WHERE
+            j.JOB_ID = e.JOB_ID) AS Cargo,
+    ROUND(AVG(salary), 2) AS 'Média salarial',
+    CASE
+        WHEN ROUND(AVG(salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        WHEN ROUND(AVG(salary), 2) > 10500 THEN 'CEO'
+    END AS 'Senioridade'
+FROM
+    employees e
+GROUP BY Cargo
+ORDER BY ROUND(AVG(salary), 2);
+    
