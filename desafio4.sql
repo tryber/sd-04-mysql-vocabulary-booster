2c31308d6aa3aa04042aa92e83c884039e1e78b9
Julio Cesar <stininmatic@hotmail.com>
2020-08-26 15:25:10 -0300

    E lá vamos nós

--- desafio4.sql
+++ desafio4.sql
@@ -1,12 +1,12 @@
 select * from hr.employees;
 
-select j.JOB_TITLE as Cargo, round(AVG(e.salary),2) 'Média Salarial', (
+select j.job_title as Cargo, round(AVG(e.salary),2) 'Média Salarial', 
 case
 when round(AVG(e.salary),2) between 2000 and 5800 then 'Júnior'
 when round(AVG(e.salary),2) between 5801 and 7500 then 'Pleno'
 when round(AVG(e.salary),2) between 7501 and 10500 then 'Sênior'
 else 'CEO'
-end) Senioridade
+end Senioridade
 from hr.jobs j
 inner join hr.employees e
 on e.JOB_ID = j.JOB_ID
