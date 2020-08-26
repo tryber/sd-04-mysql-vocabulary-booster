afb8e28f45049d76606f4f490a1f317a22e5190f
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-26 15:20:03 -0300

    desafio7?

--- desafio7.sql
+++ desafio7.sql
@@ -1,10 +1,10 @@
 SELECT 
     UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) `Nome completo`,
-    jh.START_DATE `Data de início do cargo`,
+    jh.START_DATE `Data de início`,
     e.SALARY `Salário`
 FROM
     hr.employees e
         INNER JOIN
     hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
         AND MONTH(jh.START_DATE) <= (3)
-ORDER BY `Nome completo` ASC , `Data de início do cargo` ASC;
+ORDER BY `Nome completo` ASC , `Data de início` ASC;
