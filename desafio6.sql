88b305b7c38ad53b36191dbcd68f4c26517e4b98
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-26 16:21:41 -0300

    fixed error

--- desafio6.sql
+++ desafio6.sql
@@ -1,4 +1,5 @@
-select concat(e.first_name," ",e.last_name) as 'Nome completo', j.job_title, e.hire_date as 'Data de início do cargo', d.DEPARTMENT_NAME as 'Departamento' 
-from jobs j, employees e, departments d
-where e.JOB_ID = j.JOB_ID and e.DEPARTMENT_ID = d.DEPARTMENT_ID
+select concat(e.first_name," ",e.last_name) as 'Nome completo', j.job_title, jh.START_DATE as 'Data de início do cargo', d.DEPARTMENT_NAME as 'Departamento' 
+from jobs j, employees e, departments d, job_history jh
+where e.JOB_ID = j.JOB_ID and e.DEPARTMENT_ID = d.DEPARTMENT_ID and e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 order by 1 desc, 2;
+
--- desafio7.sql
+++ desafio7.sql
@@ -1,1 +1,3 @@
-select ucase(concat(first_name," ",last_name)) as 'Nome completo', hire_date as 'Data de início', salary as Salário from employees order by 1, 2 ;
+select ucase(concat(e.first_name," ",e.last_name)) as 'Nome completo', jh.START_DATE as 'Data de início', salary as Salário 
+from employees e, job_history jh 
+where e.EMPLOYEE_ID = jh.EMPLOYEE_ID order by 1, 2 ;
