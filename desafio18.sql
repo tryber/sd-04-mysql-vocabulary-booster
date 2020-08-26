1a4d6dcc33c3d3b3ba9f3c63d6ae180f22f972ea
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-08-26 00:33:50 -0300

    Almost Everything fixed

--- desafio18.sql
+++ desafio18.sql
@@ -6,7 +6,7 @@ RETURNS INT READS SQL DATA
 BEGIN
   DECLARE total INT;
   SELECT COUNT(*) FROM hr.employees
-  WHERE MONTH(hire_date) = 6 AND YEAR(hire_date) = 1987
+  WHERE MONTH(hire_date) = mes AND YEAR(hire_date) = ano INTO total;
   RETURN total;
 END $$
 
--- desafio19.sql
+++ desafio19.sql
@@ -1,7 +1,7 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN searchEmail INT)
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN searchEmail VARCHAR(100))
 BEGIN
 SELECT CONCAT(first_name, ' ', last_name) AS 'Nome completo',
   department_name AS 'Departamento',
@@ -10,8 +10,8 @@ FROM hr.job_history AS h
   JOIN hr.employees AS e ON h.employee_id = e.employee_id
   JOIN hr.departments AS d ON d.department_id = e.department_id
   JOIN hr.jobs AS j ON j.job_id = e.job_id
-ORDER BY department_name, job_title;
 WHERE email = searchEmail
+ORDER BY department_name, job_title;
 END $$
 
 DELIMITER ;
