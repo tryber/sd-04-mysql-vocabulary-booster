b67295e334e453b794bceb7a5a8193c85f6569a9
henrique <hbaetaleite@gmail.com>
2020-08-26 18:50:12 -0300

    desafio 14 corrigindo nome da coluna

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-SELECT DISTINCT Country FROM w3schools.suppliers
+SELECT DISTINCT Country AS `País` FROM w3schools.suppliers
 UNION
-SELECT DISTINCT Country FROM w3schools.customers
-ORDER BY Country
+SELECT DISTINCT Country AS `País` FROM w3schools.customers
+ORDER BY `País`
 LIMIT 5;
