2c70ef69a160866d4086b119282523bf67a079b4
henrique <hbaetaleite@gmail.com>
2020-08-26 13:34:28 -0300

    desafio 7 chorando!

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,7 @@
-SELECT UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS `Nome Completo`,
-his.START_DATE  AS `Data de início`,
-emp.SALARY AS 'Salário'
+SELECT UCASE(CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME)) AS `Nome completo`,
+his.start_date AS `Data de início`,
+emp.salary AS 'Salário'
 FROM hr.employees AS emp
-INNER JOIN hr.job_history AS his
-ON emp.employee_id = his.employee_id
-WHERE MONTH(his.START_DATE) IN (1,2,3)
-ORDER BY `Nome Completo`, `Data de início`;
+INNER JOIN job_history AS his ON emp.employee_id = his.employee_id
+WHERE MONTH(his.start_date) IN (1, 2, 3)
+ORDER BY `Nome completo` , `Data de início`;
