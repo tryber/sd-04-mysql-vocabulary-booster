c66a0076f3daa29d8136185aab32ccee2a6ef369
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-26 18:17:03 -0300

    desafios 12 e 13

---
+++ desafio12.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) AS 'Nome completo funcionário 1',
+    A.SALARY AS 'Salário funcionário 1',
+    A.PHONE_NUMBER AS 'Telefone funcionário 1',
+    CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME) AS 'Nome completo funcionário 2',
+    B.SALARY AS 'Salário funcionário 2',
+    B.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM
+    hr.employees AS A
+        INNER JOIN
+    hr.employees AS B ON A.JOB_ID = B.JOB_ID
+WHERE
+    A.FIRST_NAME <> B.FIRST_NAME
+        AND A.LAST_NAME <> B.LAST_NAME
+ORDER BY 1 , 4;
