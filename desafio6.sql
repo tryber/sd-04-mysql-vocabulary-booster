7de2168e22768eeb6e34558f91980aa3eca7248c
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-25 21:53:29 -0300

    desafio 6 canseira

--- desafio6.sql
+++ desafio6.sql
@@ -23,7 +23,7 @@ FROM
         INNER JOIN
     hr.jobs AS J ON H.JOB_ID = J.JOB_ID
         INNER JOIN
-    hr.employees AS E ON E.EMPLOYEE_ID = E.EMPLOYEE_ID
+    hr.employees AS E ON E.EMPLOYEE_ID = H.EMPLOYEE_ID
         INNER JOIN
     hr.departments AS D ON H.DEPARTMENT_ID = D.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC , `Cargo` ASC;
