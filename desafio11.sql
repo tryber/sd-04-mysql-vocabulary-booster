b28c9ca786972916588e30cc7fde49c1ad724a27
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-26 16:45:02 -0300

    CC

--- desafio11.sql
+++ desafio11.sql
@@ -7,8 +7,7 @@ SELECT
             w3schools.customers AS B
         WHERE
             A.Country = B.Country
-        GROUP BY Country
-	) AS 'Número de compatriotas'
+        GROUP BY Country) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS A
 ORDER BY ContactName;
