2399f4f982bb5c0be7bcc575bfcc1c7e7054ed25
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-26 18:19:29 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT * FROM w3schools.products;
+SELECT * FROM w3schools.order_details;
+
+SELECT p.ProductName `Produto`,
+MIN(od.Quantity) `Mínima`,
+MAX(od.Quantity) `Máxima`,
+ROUND(AVG(od.Quantity), 2) `Média`
+FROM w3schools.products p
+INNER JOIN w3schools.order_details od ON p.ProductID = od.ProductID
+GROUP BY `Produto` HAVING `Média` > 20.00
+ORDER BY `Média` ASC, `Produto` ASC;
