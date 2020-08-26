90f000e3622385df20a2fe4f73fb4c65ac81550c
Julio Cesar <stininmatic@hotmail.com>
2020-08-26 15:41:12 -0300

    Se não passar, vou pra T5

--- desafio4.sql
+++ desafio4.sql
@@ -1,8 +1,8 @@
 select j.job_title as Cargo, round(AVG(e.salary),2) 'Média Salarial', 
 case
-when round(AVG(e.salary),2) between 2000 and 5800 then 'Júnior'
-when round(AVG(e.salary),2) between 5801 and 7500 then 'Pleno'
-when round(AVG(e.salary),2) between 7501 and 10500 then 'Sênior'
+when AVG(e.salary) between 2000 and 5800 then 'Júnior'
+when AVG(e.salary) between 5801 and 7500 then 'Pleno'
+when AVG(e.salary) between 7501 and 10500 then 'Sênior'
 else 'CEO'
 end Senioridade
 from hr.jobs j
