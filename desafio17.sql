5a48722257d0e9c068ca5032377c80e8c8aa7cee
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-26 15:24:07 -0300

    17 cc

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 SELECT
   CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
-  jh.start_date AS 'Data de início',
-  jh.end_date AS 'Data de recisão',
+  DATE_FORMAT(jh.start_date, '%d/%m/%y') AS 'Data de início',
+  DATE_FORMAT(jh.end_date, '%d/%m/%y') AS 'Data de recisão',
   ROUND(DATEDIFF(jh.end_date, jh.start_date) / 365, 2) AS 'Anos trabalhados'
 FROM
   hr.job_history AS jh
