aec521f22192cc8babde5958c787f3c9c947b64e
Caio Magalhães <caio.lmf@gmail.com>
2020-08-26 15:15:09 -0300

    CodeClimate challenge 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,5 @@
 SELECT J.JOB_TITLE AS 'Cargo',
-(J.MAX_SALARY - J.MIN_SALARY) as 'Diferença entre salários máximo e mínimo'
+    (J.MAX_SALARY - J.MIN_SALARY) as 'Diferença entre salários máximo e mínimo'
 FROM jobs AS J
-ORDER BY (J.MAX_SALARY - J.MIN_SALARY), J.JOB_TITLE;
+ORDER BY (J.MAX_SALARY - J.MIN_SALARY),
+    J.JOB_TITLE;
