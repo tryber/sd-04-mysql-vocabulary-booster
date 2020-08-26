353a332fef7b8fecd5b9eef621420059544320e9
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-26 15:38:17 -0300

    desafio 12

--- desafio12.sql
+++ desafio12.sql
@@ -13,12 +13,17 @@
 -- Em caso de empate, os resultados devem ser ordenados pela coluna Nome completo funcionário 2" em ordem alfabética.
 
 SELECT 
-CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) AS 'Nome completo funcionário 1',
-A.SALARY AS 'Salário funcionário 1',
-A.PHONE_NUMBER AS 'Telefone funcionário 1',
-CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME) AS 'Nome completo funcionário 2',
-B.SALARY AS 'Salário funcionário 2',
-B.PHONE_NUMBER AS 'Telefone funcionário 2'
+    CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) AS 'Nome completo funcionário 1',
+    A.SALARY AS 'Salário funcionário 1',
+    A.PHONE_NUMBER AS 'Telefone funcionário 1',
+    CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME) AS 'Nome completo funcionário 2',
+    B.SALARY AS 'Salário funcionário 2',
+    B.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM
-hr.employees AS A,  hr.employees AS B WHERE A.JOB_ID = B.JOB_ID AND 'Nome completo funcionário 1' <> 'Nome completo funcionário 2'
-ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
+    hr.employees AS A,
+    hr.employees AS B
+WHERE
+    A.JOB_ID = B.JOB_ID
+        AND CONCAT(A.FIRST_NAME, ' ', A.LAST_NAME) <> CONCAT(B.FIRST_NAME, ' ', B.LAST_NAME)
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;
+
