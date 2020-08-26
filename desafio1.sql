2705a78e53163e839eebb4ddfd2d4a407801c542
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-25 22:00:21 -0300

    detalhes do avaliador ok

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,6 @@
 SELECT t1.COUNTRY_NAME AS 'País', 
-IF(t2.REGION_ID = 1, 'incluido', 'não incluido' ) AS 'Status Inclusão'
+IF(t2.REGION_ID = 1, 'incluído', 'não incluído' ) AS 'Status Inclusão'
 FROM hr.regions AS t2
 INNER JOIN hr.countries AS t1
+GROUP BY t1.COUNTRY_NAME
 ORDER BY t1.COUNTRY_NAME;
