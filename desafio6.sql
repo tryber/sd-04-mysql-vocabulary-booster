1dec13ffc36119861aa98a2921162ad3353c1280
yoshidalucas92 <lucaslimayoshida@gmail.com>
2020-08-25 21:25:36 -0300

    obrigado Ronan kkkk

--- desafio6.sql
+++ desafio6.sql
@@ -5,6 +5,6 @@ h.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-INNER JOIN hr.job_history AS h ON e.JOB_ID = h.JOB_ID
+INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 INNER JOIN hr.departments AS d ON h.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
