1801c30a7205da8729707236e78e2760cf14318c
juniomelos <juniomelos@gmail.com>
2020-08-26 09:56:12 -0300

    desafio 15 e 16 -  não esta retornando o esperado - CC01

--- desafio15.sql
+++ desafio15.sql
@@ -4,12 +4,14 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN jobTitle VARCHAR(50))
 
 BEGIN
-	SELECT ROUND(AVG(emp.SALARY), 2) AS `Média Salarial` FROM hr.employees AS emp
-    JOIN hr.jobs AS jobs
-    ON jobs.JOB_ID = emp.JOB_ID
-    WHERE jobs.JOB_TITLE = jobTitle
-    GROUP BY jobTitle
-    ORDER BY `Média Salarial`, jobTitle;
+
+SELECT ROUND(AVG(emp.SALARY), 2) AS `Média Salarial` FROM hr.employees AS emp
+JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = emp.JOB_ID
+WHERE jobs.JOB_TITLE = jobTitle
+GROUP BY jobTitle
+ORDER BY `Média Salarial`, jobTitle;
+
 END $$
 
 DELIMITER ;
--- desafio16.sql
+++ desafio16.sql
@@ -5,15 +5,17 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100)
 RETURNS INT READS SQL DATA
 
 BEGIN
-    DECLARE amount_jobs_history INT;
-    SELECT COUNT(jh.EMPLOYEE_ID)
-	FROM hr.employees AS emp
-    JOIN hr.job_history AS jh
-    ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
-	WHERE emp.EMAIL = email
-	INTO amount_jobs_history;
-	RETURN amount_jobs_history;
-	END $$
+
+DECLARE amount_jobs_history VARCHAR(100);
+SELECT COUNT(jh.EMPLOYEE_ID)
+FROM hr.employees AS emp
+JOIN hr.job_history AS jh
+ON jh.EMPLOYEE_ID = emp.EMPLOYEE_ID
+WHERE emp.EMAIL = email
+INTO amount_jobs_history;
+RETURN amount_jobs_history;
+
+END $$
 
 DELIMITER ;
 
