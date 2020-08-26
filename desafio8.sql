7ef809c237d777a14b1af043d87c7f768bcfa6c2
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-25 23:12:15 -0300

    agora passa o 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,8 @@
-SELECT
-t3.ContactName AS 'Nome de contato',
-t2.ShipperName AS 'Empresa que fez o envio',
-t1.OrderDate AS 'Data do pedido'
+SELECT t3.ContactName AS `Nome de contato`,
+t2.ShipperName AS `Empresa que fez o envio`,
+t1.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS t1
 INNER JOIN w3schools.shippers AS t2 ON t1.ShipperID = t2.ShipperID
-INNER JOIN w3schools.customers AS t3 ON t1.CustomerID = t2.CustomerID
-WHERE t2.ShipperName IN ('Speedy Express', 'United Package')
+INNER JOIN w3schools.customers AS t3 ON t1.CustomerID = t3.CustomerID
+WHERE t2.ShipperName IN ('Speedy Express' , 'United Package')
 ORDER BY t3.ContactName, t2.ShipperName, t1.OrderDate;
