17bb0b0b6c0654e5ccb70e031987f8e1cb94091e
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-26 00:06:50 -0300

    desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -13,7 +13,7 @@
 SELECT 
     UCASE(CONCAT(E.FIRST_NAME, '  ', E.LAST_NAME)) AS `Nome completo`,
     H.START_DATE AS `Data de início`,
-    E.SALARY AS `Salário`
+    E.SALARY AS 'Salário'
 FROM
     hr.employees AS E
         INNER JOIN
