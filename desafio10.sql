74e0dc140482264ca3ebc765b78ea6216d0b00af
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-26 18:43:43 -0300

    desafio 10 - 03

--- desafio10.sql
+++ desafio10.sql
@@ -2,11 +2,13 @@ SELECT
   ProductID AS `Produto`,
   MIN(Quantity) AS `Mínima`,
   MAX(Quantity) AS `Máxima`,
-  AVG (Quantity) AS `Média`
+  ROUND(AVG (Quantity), 2) AS `Média`
 FROM
   w3schools.order_details
 GROUP BY
   ProductID
+HAVING
+  `Média` > 20
 ORDER BY
   `Média`,
   `Produto`;
