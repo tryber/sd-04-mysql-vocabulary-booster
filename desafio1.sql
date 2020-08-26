a6c14fe71165e840a0734a7426c2a75e4f807afa
Caio Magalhães <caio.lmf@gmail.com>
2020-08-26 14:58:22 -0300

    do challenge  1

---
+++ desafio1.sql
@@ -0,0 +1,9 @@
+SELECT C.COUNTRY_NAME AS 'País',
+    IF(
+        R.REGION_NAME = 'Europe',
+        'incluído',
+        'não incluído'
+    ) AS 'Status Inclusão'
+FROM countries AS C
+    INNER JOIN regions AS R ON (C.REGION_ID = R.REGION_ID)
+ORDER BY C.COUNTRY_NAME;
