2f25d820b5e8a87ab5938e6d64783d5831dfb00a
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-08-26 15:54:56 -0300

    testando erro cypress

--- desafio16.sql
+++ desafio16.sql
@@ -1,9 +1,12 @@
+USE hr;
+DROP function IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
+
 DELIMITER $$
 USE hr$$
-CREATE DEFINER=CURRENT_USER FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_employee varchar(25)) RETURNS int
+CREATE DEFINER=CURRENT_USER FUNCTION `buscar_quantidade_de_empregos_por_funcionario`(email_employee varchar(25)) RETURNS int
     READS SQL DATA
 BEGIN
-DECLARE total_emails INTEGER;
+DECLARE total_empregos INTEGER;
 SELECT 
 count(e.LAST_NAME) INTO total_empregos
 FROM hr.employees e 
