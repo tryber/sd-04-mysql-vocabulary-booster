20df8dd0bd4557fa08ddad756e8d7c0c6a3c7530
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 23:42:55 -0300

    req 10 denovo

--- desafio10.sql
+++ desafio10.sql
@@ -1,5 +1,3 @@
-select * FROM products; select * FROM orders; select * FROM order_details ORDER BY ProductID;
-
 SELECT P.ProductName Produto, (MIN(OD.Quantity)) Mínima, 
 (MAX(OD.Quantity)) Máxima, ROUND(AVG(OD.Quantity), 2) Média
 FROM w3schools.products P
