1f2bc4338d2e0921859fffcab2da0a36c87a9fa7
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-08-26 15:06:25 -0300

    15 feito, talvez

---
+++ desafio15.sql
@@ -0,0 +1,16 @@
+USE hr;
+DROP procedure IF EXISTS buscar_media_por_cargo;
+
+DELIMITER $$
+USE hr$$
+CREATE PROCEDURE `buscar_media_por_cargo`(IN nome_cargo varchar(35))
+BEGIN
+SELECT
+ROUND(AVG(e.SALARY), 2) "Média salarial"
+FROM hr.employees e
+INNER JOIN hr.jobs j 
+ON j.JOB_ID = e.JOB_ID
+WHERE JOB_TITLE = nome_cargo;
+END$$
+
+DELIMITER ;
