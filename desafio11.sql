ca9c4326570784c97051182a7b301cc005b541e0
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-26 19:34:23 -0300

    removing query at desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,3 @@
-SELECT * FROM w3schools.customers;
 SELECT a.ContactName `Nome`, a.Country `País`, (SELECT Count(a.Country)-1) `Número de compatriotas`
 FROM w3schools.customers a, w3schools.customers b WHERE a.Country = b.Country
 GROUP BY `Nome`, `País`
