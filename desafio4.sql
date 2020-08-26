5a2fca3e8889f7ebd57496ce68934915041b8f8f
Lucas Ribas <lucas@mundoribas.com.br>
2020-08-26 14:24:19 -0300

    Desafios 5 e 6

--- desafio4.sql
+++ desafio4.sql
@@ -1,16 +1,30 @@
-SELECT JOB_TITLE AS Cargo,
-  (
-    SELECT ROUND(AVG(SALARY), 2)
-    FROM hr.employees
-    WHERE employees.JOB_ID = jobs.JOB_ID
-    GROUP BY JOB_ID
-  ) AS 'Média salarial',
+SELECT j.JOB_TITLE AS Cargo,
+  ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
   CASE
-    WHEN 'Média salarial' BETWEEN 2000 AND 5800 THEN 'Júnior'
-    WHEN 'Média salarial' BETWEEN 5801 AND 7500 THEN 'Pleno'
-    WHEN 'Média salarial' BETWEEN 7501 AND 10500 THEN 'Sênior'
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(e.SALARY), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
     ELSE 'CEO'
-  END AS Senioridade
-FROM hr.jobs
-ORDER BY 'Média salarial' ASC,
-  cargo ASC;
+  END AS 'Senioridade'
+FROM hr.jobs AS j
+  INNER JOIN hr.employees AS e ON j.JOB_ID = e.JOB_ID
+GROUP BY j.JOB_TITLE
+ORDER BY ROUND(AVG(e.SALARY), 2),
+  Cargo;
+
+-- SELECT JOB_TITLE AS Cargo,
+--   (
+--     SELECT ROUND(AVG(SALARY), 2)
+--     FROM hr.employees
+--     WHERE employees.JOB_ID = jobs.JOB_ID
+--     GROUP BY JOB_ID
+--   ) AS 'Média salarial',
+--   CASE
+--     WHEN 'Média salarial' BETWEEN 2000 AND 5800 THEN 'Júnior'
+--     WHEN 'Média salarial' BETWEEN 5801 AND 7500 THEN 'Pleno'
+--     WHEN 'Média salarial' BETWEEN 7501 AND 10500 THEN 'Sênior'
+--     ELSE 'CEO'
+--   END AS Senioridade
+-- FROM hr.jobs
+-- ORDER BY 'Média salarial' ASC,
+--   cargo ASC;
