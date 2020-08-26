0aa084d38c4838cd7a8de3b285e71f7b91f860f2
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 21:58:50 -0300

    req 8 reenviando

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,3 @@
-select * from customers order by ContactName; select * from shippers; select * from order_details; select * from orders where customerID = 31;
-
 select C.ContactName 'Nome de contato', S.ShipperName 'Empresa que fez o envio', O.OrderDate 'Data do pedido'
 FROM w3schools.customers C
 INNER JOIN w3schools.orders O on C.CustomerID = O.CustomerID
