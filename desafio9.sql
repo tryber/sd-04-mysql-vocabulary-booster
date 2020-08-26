758ba2114fe8f3f3eaa4ffd1b36b6be5532f42fd
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-26 01:43:50 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,20 @@
+SELECT * FROM w3schools.employees;
+-- 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu
+-- total de pedidos feitos. Usando o banco w3schools como referência, monte uma query que
+-- exiba duas colunas:
+
+-- A primeira coluna deve possuir o alias "Nome completo" e exibir o nome completo da pessoa
+-- funcionária (não se esqueça do espaço entre o nome e o sobrenome).
+
+-- A segundo coluna deve possuir o alias "Total de pedidos" e exibir a quantidade total de
+-- pedidos feitos pela pessoa.
+
+-- Ordene seus resultados pelo total de pedidos em ordem crescente.
+SELECT 
+    CONCAT(E.FirstName,'  ',E.LastName) AS 'Nome completo',
+    COUNT(O.EmployeeID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS E
+        INNER JOIN
+    w3schools.orders AS O ON O.EmployeeID = E.EmployeeID GROUP BY O.EmployeeID ORDER BY `Total de pedidos`;
+    
