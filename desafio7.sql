1452c85b81e0b0827a423bde813ba36f0b139f21
Pedro <pedrotpoliveira@gmail.com>
2020-08-26 00:06:02 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT UCASE(CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME)) AS `Nome completo`,
+jh.START_DATE AS `Data de início do cargo`,
+em.SALARY AS `Salário`
+FROM hr.job_history AS jh
+INNER JOIN hr.jobs AS jb
+ON jh.JOB_ID=jb.JOB_ID
+INNER JOIN hr.employees AS em
+ON em.EMPLOYEE_ID=jh.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) IN (01, 02, 03) 
+ORDER BY `Nome completo`, `Data de início do cargo`;
