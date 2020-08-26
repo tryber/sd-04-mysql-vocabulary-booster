f4ee2b9b1d6622601b4febfc4594e2346f7108df
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-08-26 15:36:38 -0300

    16 pronto

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DROP function IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
+
+DELIMITER $$
+USE hr$$
+CREATE DEFINER=CURRENT_USER FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_employee varchar(25)) RETURNS int
+    READS SQL DATA
+BEGIN
+DECLARE total_emails INTEGER;
+SELECT 
+count(e.LAST_NAME) INTO total_emails
+FROM hr.employees e 
+INNER JOIN job_history jh 
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID 
+WHERE e.EMAIL = email_employee;
+RETURN total_emails;
+END$$
