d1da4be90ab4cff66041a683be011aca86688af6
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-08-26 17:25:33 -0300

    cc fix

--- desafio2.sql
+++ desafio2.sql
@@ -9,21 +9,4 @@ SELECT
 FROM
     jobs
 ORDER BY Cargo;
-
-/*SELECT 
-    (SELECT 
-            job_title
-        FROM
-            jobs j
-        WHERE
-            j.JOB_ID = e.JOB_ID) AS Cargo,
-    CASE
-        WHEN max(salary) BETWEEN 0 AND 10000 THEN 'Baixo'
-        WHEN max(salary) BETWEEN 10001 AND 20000 THEN 'Médio'
-        WHEN max(salary) BETWEEN 20001 AND 30000 THEN 'Alto'
-        WHEN max(salary) > 30000 THEN 'Altíssimo'
-        ELSE 'Usuário sem nível'
-    END AS 'Nível'
-FROM
-    employees e group by Cargo;*/
     
