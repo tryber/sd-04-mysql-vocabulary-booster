2e05bbe644340be51913aabdb90e8955bb6b465f
leandromarten1 <leandromarten1@gmail.com>
2020-08-26 15:05:39 -0300

    Desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,22 @@
+-- DESAFIO 16
+
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email_func VARCHAR(200))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE qnt_empregos INT;
+    SELECT COUNT(JH.employee_id)
+	FROM hr.job_history AS JH
+	INNER JOIN hr.employees AS E
+	ON JH.employee_id = E.employee_id
+	WHERE E.email = email_func
+    INTO qnt_empregos;
+    RETURN qnt_empregos;
+END $$
+
+DELIMITER ;
+
+-- DROP FUNCTION buscar_quantidade_de_empregos_por_funcionario;
+-- SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
