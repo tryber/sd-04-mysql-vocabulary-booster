a15f5276f5c463917e105f84220104696622bd4c
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-08-26 14:25:49 -0300

    ???

--- desafio11.sql
+++ desafio11.sql
@@ -5,5 +5,5 @@ FROM w3schools.customers AS a,
   w3schools.customers AS b
 WHERE a.country = b.country
   AND a.contactName <> b.contactName
-GROUP BY a.contactName
+GROUP BY a.customerID
 ORDER BY a.contactName;
