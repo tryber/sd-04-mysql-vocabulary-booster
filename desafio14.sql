21e05bf44f6c7958bcba0c86c0db1b2e8aabae6d
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-08-25 23:03:23 -0300

    req14C

---
+++ desafio14.sql
@@ -0,0 +1,17 @@
+(
+  SELECT
+    DISTINCT Country AS 'País'
+  FROM
+    w3schools.suppliers
+)
+UNION
+(
+  SELECT
+    DISTINCT Country AS 'País'
+  FROM
+    w3schools.customers
+)
+ORDER BY
+  `País`
+LIMIT
+  5;
