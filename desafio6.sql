4e2cb19653b900ce092598470d345c516e5d5d40
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-08-26 14:27:06 -0300

    tirei o comentario

--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
   j.JOB_TITLE AS Cargo,
-  h.START_DATE AS 'Data de início do cargo',
+  h.START_DATE AS `Data de início do cargo`,
   d.DEPARTMENT_NAME AS Departamento
   FROM hr.job_history AS h
   INNER JOIN hr.jobs AS j
