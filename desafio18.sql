635048e0e41c919238ffab6d18037ab8eec3f9df
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-08-26 16:39:54 -0300

    18

--- desafio18.sql
+++ desafio18.sql
@@ -1,23 +1,22 @@
-SET
-  GLOBAL log_bin_trust_function_creators = 1;
-
 USE hr; 
 
 DELIMITER 
+
 DROP FUNCTION IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT) RETURNS INT BEGIN 
 DECLARE quantidade_de_pessoas_contratadas INT;
 SELECT
   COUNT(*) INTO quantidade_de_pessoas_contratadas
 FROM
-  hr.job_history
+  hr.employees
 WHERE
-  MONTH(start_date) = mes
-  AND YEAR(start_date) = ano;
+  MONTH(hire_date) = mes
+  AND YEAR(hire_date) = ano;
 
 RETURN quantidade_de_pessoas_contratadas;
 
-END; 
+END;
+
 DELIMITER ;
 
 SELECT
