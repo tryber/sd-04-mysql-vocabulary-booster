2833c41b9d3f77a3b1984019b211edfecbe5ab97
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-26 16:56:53 -0300

    desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,6 @@
-SELECT * FROM w3schools.employees;
-SELECT * FROM w3schools.orders;
-SELECT * FROM w3schools.order_details;
-SELECT 
-(SELECT CONCAT(FirstName, ' ', LastName) fullname FROM w3schools.employees e 
-WHERE e.EmployeeID = o.EmployeeID) `Nome completo`,
-(SELECT Quantity FROM w3schools.order_details od 
-WHERE od.OrderID = o.OrderID AND o.EmployeeID = e.EmployeeID) `Total de pedidos` 
-FROM w3schools.orders o, w3schools.employees e 
-group by `Nome completo`
-order by `Nome completo`;
+SELECT CONCAT(e.FirstName, ' ', e.LastName) `Nome completo`,
+COUNT(o.OrderID) `Total de pedidos`
+FROM w3schools.employees e
+INNER JOIN  w3schools.orders o ON e.EmployeeID = o.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`; 
