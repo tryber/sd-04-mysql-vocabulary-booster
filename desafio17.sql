46408740992f5c3f5cf8bf8a0604e5ac7365a451
juniomelos <juniomelos@gmail.com>
2020-08-26 16:59:02 -0300

    desafio 17 - sempre esqueço de salvar

--- desafio17.sql
+++ desafio17.sql
@@ -1,6 +1,6 @@
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
-DATE_FORMAT(jh.START_DATE, '%d/%m/%y') AS `Data de início`,
-DATE_FORMAT(jh.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
+DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS `Data de início`,
+DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
 ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE)/365, 2) AS `Anos trabalhados`
 FROM 
 hr.employees AS emp,
