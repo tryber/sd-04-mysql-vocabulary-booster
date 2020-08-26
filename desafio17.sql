d5162e7a67e0b31f0e4aec38d2a20050008543b7
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-08-26 14:31:55 -0300

    fixing date format

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
-CONCAT(DAY(JH.START_DATE),'/', MONTH(JH.START_DATE),'/', YEAR(JH.START_DATE)) AS `Data de início`,
-CONCAT(DAY(JH.END_DATE),'/', MONTH(JH.END_DATE),'/', YEAR(JH.END_DATE)) AS `Data de rescisão`,
+DATE_FORMAT(JH.START_DATE, "%d/%m/%Y") AS `Data de início`,
+DATE_FORMAT(JH.END_DATE, "%d/%m/%Y") AS `Data de rescisão`,
 ROUND(DATEDIFF(JH.END_DATE, JH.START_DATE)/365,2) AS `Anos trabalhados`
 FROM hr.job_history AS JH
 INNER JOIN hr.employees AS E ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
