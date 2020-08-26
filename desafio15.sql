176c1dca651e848268bb89c877cf93b24454e65f
igorgbr <igorgphotoarte@gmail.com>
2020-08-26 20:54:23 -0300

    desafio 15 CC

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,8 @@
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo_funcionario VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(E.salary), 2) 'Média salarial'
-	FROM hr.jobs J 
-	INNER JOIN hr.employees E on E.JOB_ID = J.JOB_ID
-	GROUP BY J.JOB_TITLE 
-    HAVING J.JOB_TITLE = cargo_funcionario;
+SELECT ROUND(AVG(E.salary), 2) 'Média salarial'
+FROM hr.jobs J 
+INNER JOIN hr.employees E on E.JOB_ID = J.JOB_ID
+GROUP BY J.JOB_TITLE 
+HAVING J.JOB_TITLE = cargo_funcionario;
 END $$
