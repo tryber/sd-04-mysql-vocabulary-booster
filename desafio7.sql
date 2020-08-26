8c4bfe967bbf16ad1a315b41caf9804d3b577663
igorgbr <igorgphotoarte@gmail.com>
2020-08-25 21:07:49 -0300

    req 7 café quero café

--- desafio7.sql
+++ desafio7.sql
@@ -1,4 +1,4 @@
-SELECT UPPER(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) 'Nome completo', J.JOB_TITLE Cargo, JH.START_DATE 'Data de início', E.SALARY Salário
+SELECT UPPER(CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME)) 'Nome completo', JH.START_DATE 'Data de início', E.SALARY Salário
 FROM hr.employees E
 INNER JOIN hr.job_history JH on JH.EMPLOYEE_ID = E.EMPLOYEE_ID
 INNER JOIN hr.jobs J on J.JOB_ID = JH.JOB_ID
