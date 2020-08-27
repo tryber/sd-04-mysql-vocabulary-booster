8c1ddb647aa46af6f481f90b74c40060f8d29c5b
Tiago-ers <trsilva284@gmail.com>
2020-08-27 13:51:04 -0300

    desafio12 refatorando 2

--- desafio12.sql
+++ desafio12.sql
@@ -3,5 +3,5 @@ A.SALARY AS 'Salário funcionário 1', A.PHONE_NUMBER AS 'Telefone funcionário 
 CONCAT_WS(' ', B.FIRST_NAME, B.LAST_NAME) AS 'Nome completo funcionário 2',
 B.SALARY AS 'Salário funcionário 2', B.PHONE_NUMBER AS 'Telefone funcionário 2' FROM employees AS A
 INNER JOIN employees AS B ON A.JOB_ID = B.JOB_ID 
-AND A.SALARY <> B.SALARY
+AND A.EMPLOYEE_ID <> B.EMPLOYEE_ID
 ORDER BY CONCAT_WS(' ', A.FIRST_NAME, A.LAST_NAME), CONCAT_WS(' ', B.FIRST_NAME, B.LAST_NAME);
