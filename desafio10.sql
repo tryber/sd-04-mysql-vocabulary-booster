9928f490324096e6bbcff54c22567dde9cccda09
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:42:35 -0300

    Update desafio10.sql

--- desafio10.sql
+++ desafio10.sql
@@ -4,7 +4,8 @@ SELECT
   MAX(od.Quantity) AS `Máxima`,
   ROUND(AVG(od.Quantity), 2) AS `Média`
 FROM w3schools.order_details AS od
-  INNER JOIN w3schools.products AS p ON p.ProductID = od.ProductID
-    group by `Produto`
-      HAVING `Média` > 20
-    ORDER BY `Média`, `Produto`;
+  JOIN w3schools.products AS p
+    ON p.ProductID = od.ProductID
+GROUP BY `Produto`
+  HAVING `Média` > 20
+ORDER BY `Média`, `Produto`;
