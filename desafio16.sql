40abee497e14598b90124299392a3ce6685daaca
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-27 01:28:09 -0300

    try to fixed it

--- desafio16.sql
+++ desafio16.sql
@@ -1,8 +1,7 @@
 delimiter $$
 create procedure buscar_quantidade_de_empregos_por_funcionario(in func_email varchar(100))
 begin
-select count(EMPLOYEE_ID) as 'quantidade de empregos presentes em seu histórico' from job_history where EMPLOYEE_ID in
-(select EMPLOYEE_ID from employees where EMAIL = func_email);
+select count(EMPLOYEE_ID) as 'quantidade de empregos presentes em seu histórico' from job_history where EMPLOYEE_ID in (select EMPLOYEE_ID from employees where EMAIL = func_email);
 end $$
 delimiter ;
 call buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
