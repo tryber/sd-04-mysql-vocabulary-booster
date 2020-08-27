b66f3a739638007141e29f392fdd0b3d147f417c
igorgbr <igorgphotoarte@gmail.com>
2020-08-26 22:27:31 -0300

    desafio 17 corrigido

--- desafio17.sql
+++ desafio17.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) `Nome completo`, DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') `Data de início`, DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') `Data de recisão`, 
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) `Nome completo`, DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') `Data de início`, DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') `Data de rescisão`, 
 round((datediff(JH.END_DATE, JH.START_DATE) / 365), 2) `Anos trabalhados`
 FROM hr.employees E
 INNER JOIN hr.job_history JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
