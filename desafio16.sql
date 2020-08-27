0aa737cfa762bc1e67225a9a8070a03102160477
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-27 01:36:50 -0300

    trying ffixed again

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,7 @@
-delimiter $$
-create procedure buscar_quantidade_de_empregos_por_funcionario(in func_email varchar(100))
-begin
+DELIMITER $$
+CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN func_email varchar(100))
+BEGIN
 select count(EMPLOYEE_ID) as 'quantidade de empregos presentes em seu histórico' from job_history where EMPLOYEE_ID in (select EMPLOYEE_ID from employees where EMAIL = func_email);
-end $$
+END $$
 delimiter ;
 call buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
