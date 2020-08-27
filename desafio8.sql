6c132f3dea0b4df51eb59e03275e6790411e3ca2
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:53:03 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,6 +1,11 @@
-SELECT c.ContactName AS `Nome de contato`, s.ShipperName AS `Empresa que fez o envio`, o.OrderDate AS `Data do pedido`
+SELECT
+  c.ContactName AS `Nome de contato`,
+  s.ShipperName AS `Empresa que fez o envio`,
+  o.OrderDate AS `Data do pedido`
 FROM w3schools.customers AS c
-INNER JOIN w3schools.orders AS o ON o.CustomerID = c.CustomerID
-INNER JOIN w3schools.shippers AS s ON o.ShipperID = s.ShipperID
-WHERE s.ShipperName IN ('Speedy Express', 'United Package')
+  JOIN w3schools.orders AS o
+    ON o.CustomerID = c.CustomerID
+  JOIN w3schools.shippers AS s
+    ON o.ShipperID = s.ShipperID AND s.ShipperName IN ('Speedy Express', 'United Package')
+-- Outro caso onde WHERE não era realmente necessário    
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
