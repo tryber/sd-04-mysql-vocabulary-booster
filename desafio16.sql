8229ede35b181b3d39a31089534044458e1aa226
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-27 19:51:20 -0300

    desafio 16 - 02

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,13 @@
 USE hr;
-DELIMITER $ $ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email int) RETURNS INT READS SQL DATA BEGIN DECLARE JOB_QTY INT;
+DELIMITER $ $ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100)) RETURNS INT READS SQL DATA BEGIN DECLARE JOB_QTY INT;
 SELECT
   COUNT(SALARY)
 FROM
   hr.employees AS e
   INNER JOIN hr.job_history AS h ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
 WHERE
-  EMAIL LIKE 'NKOCHHAR' INTO JOB_QTY;
+  EMAIL LIKE email INTO JOB_QTY;
 RETURN JOB_QTY;
 END $ $ DELIMITER;
+SELECT
+  buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
