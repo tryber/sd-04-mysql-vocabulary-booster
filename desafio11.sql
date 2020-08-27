ddc191730a971dff3037116702e39009dd71d0c9
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-26 21:50:36 -0300

    desafio 11 - 01

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,17 @@
 SELECT
-  CustomerName,
-  Country
+  cu.ContactName AS `Nome`,
+  cu.Country AS `País`,
+  C.cnt - 1 AS `Número de compatriotas`
 FROM
-  w3schools.customers;
+  w3schools.customers AS cu
+  INNER JOIN (
+    SELECT
+      Country,
+      COUNT(*) AS cnt
+    FROM
+      w3schools.customers
+    GROUP BY
+      Country
+  ) AS C ON
+  cu.Country = C.Country
+  ORDER BY `Nome`;
