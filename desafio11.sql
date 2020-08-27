b8451a6f939f70d870ff117d3ce30d9e05979bb7
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-27 13:51:23 -0300

    cc

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,5 @@
-SELECT 
-a.ContactName `Nome`, a.Country `País`, (Select COUNT(a.Country)-1) `Número de compatriotas`
+SELECT a.ContactName `Nome`, a.Country `País`, COUNT(a.Country)-1 `Número de compatriotas`
 FROM w3schools.customers a, w3schools.customers b 
 WHERE a.Country = b.Country 
-GROUP BY `Nome`, `País` having (Select COUNT(a.Country)-1) <> 0
+GROUP BY `Nome`, `País` having `Número de compatriotas` <> 0
 ORDER BY `Nome`;
-
