3053ae0b3dcab812c30b3eb75f1e675427166522
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-08-27 13:41:00 -0300

    subindo solução com função pq eu queria assim

--- desafio11.sql
+++ desafio11.sql
@@ -6,3 +6,23 @@ SELECT t1.ContactName AS Nome,
   WHERE t1.Country = t2.Country AND t1.CustomerID <> t2.CustomerID
   GROUP BY t1.CustomerID
   ORDER BY Nome;
+
+-- USE w3schools;
+-- DELIMITER $$
+-- CREATE FUNCTION calcCompatriots(varCountry VARCHAR(100))
+-- RETURNS INT READS SQL DATA
+-- BEGIN
+--   DECLARE compatriots INT;
+--   SELECT COUNT(*) - 1 FROM w3schools.customers
+--   WHERE w3schools.customers.Country = varCountry
+--   INTO compatriots;
+--   RETURN compatriots;
+-- END$$
+-- DELIMITER ;
+-- SELECT
+--   ContactName AS Nome,
+--   Country AS `País`,
+--   calcCompatriots(Country) AS `Número de compatriotas`
+-- FROM w3schools.customers
+-- WHERE calcCompatriots(Country) > 0
+-- ORDER BY Nome;
