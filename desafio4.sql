2c1040bb9f9e48cc24935c7da1a466695114a4df
binhus <eng.robsonwilli@gmail.com>
2020-08-27 11:42:56 -0300

    esqueci uma coluna na 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,17 @@
-SELECT jobs.JOB_TITLE AS "Cargo", ROUND(AVG(employees.SALARY)) AS "Média salarial"
-FROM hr.jobs AS jobs
-INNER JOIN hr.employees AS employees ON (jobs.JOB_ID = employees.JOB_ID)
-GROUP BY jobs.JOB_ID
-ORDER BY `Média salarial`, Cargo;
+SELECT
+  jobs.JOB_TITLE AS "Cargo",
+  ROUND(AVG(employees.SALARY)) AS "Média salarial",
+  CASE
+    WHEN ROUND(AVG(employees.SALARY), 2) > 10500 THEN "CEO"
+    WHEN ROUND(AVG(employees.SALARY), 2) > 7500 THEN "Sênior"
+    WHEN ROUND(AVG(employees.SALARY), 2) > 5800 THEN "Pleno"
+    WHEN ROUND(AVG(employees.SALARY), 2) >= 2000 THEN "Junior"
+  END AS "Senioridade"
+FROM
+  hr.jobs AS jobs
+  INNER JOIN hr.employees AS employees ON (jobs.JOB_ID = employees.JOB_ID)
+GROUP BY
+  jobs.JOB_ID
+ORDER BY
+  `Média salarial`,
+  Cargo;
