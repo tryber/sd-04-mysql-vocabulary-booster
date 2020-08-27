ef1ad14ff3290b87b1c0a03e3f47ab5d24c1d279
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:44:18 -0300

    Update desafio12.sql

--- desafio12.sql
+++ desafio12.sql
@@ -3,7 +3,7 @@ SELECT
   CONCAT(e1.FIRST_NAME, ' ',e1.LAST_NAME) AS `Nome completo funcionário 1`,
   e1.SALARY AS `Salário funcionário 1`,
   e1.PHONE_NUMBER AS `Telefone funcionário 1`,
--- Colunas correspondentes a pessoa funcionaria 1
+-- Colunas correspondentes a pessoa funcionaria 2
   CONCAT(e2.FIRST_NAME, ' ',e2.LAST_NAME) AS `Nome completo funcionário 2`,
   e2.SALARY AS `Salário funcionário 2`,
   e2.PHONE_NUMBER AS `Telefone funcionário 2`
@@ -12,4 +12,4 @@ FROM hr.employees AS e1, hr.employees AS e2
 -- Mostra os resultados onde a primeira e a segunda pessoa são pessoas diferentes e que tem o mesmo cargo.
 WHERE e1.EMPLOYEE_ID <> e2.EMPLOYEE_ID AND e1.JOB_ID = e2.JOB_ID
 -- ordena pelo nome da pessoa1, desempata ordenando pelo nome da pessoa 2. Ordenamentos por ordem alfabetica.
-ORDER BY e1.FIRST_NAME, e1.LAST_NAME, e2.FIRST_NAME, e2.LAST_NAME;
+ORDER BY CONCAT(e1.FIRST_NAME, ' ', e1.LAST_NAME), CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME);
