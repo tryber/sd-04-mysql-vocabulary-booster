8a3756c2ddfcae3bc2e9a71cd6fb53734d8acc16
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:46:05 -0300

    Update desafio15.sql

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,16 @@
+USE hr;
+
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN Cargo VARCHAR(100))
+
 BEGIN
     SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
     FROM hr.employees AS e
-    INNER JOIN hr.jobs AS j ON e.job_id = j.JOB_ID
-    WHERE j.JOB_TITLE = Cargo;
+      JOIN hr.jobs AS j
+        ON e.job_id = j.JOB_ID AND j.JOB_TITLE = Cargo;
 END $$
 
 DELIMITER ;
