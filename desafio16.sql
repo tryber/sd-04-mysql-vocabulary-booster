cecd45189e27e3586f901c091b1a34fe1c833bfb
leonardoinatel <leonardodamasceno@gec.inatel.br>
2020-08-27 01:50:09 -0300

    fixed

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,11 @@
-DELIMITER $$
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN func_email varchar(100))
-BEGIN
-select count(EMPLOYEE_ID) as 'quantidade de empregos presentes em seu histórico' from job_history where EMPLOYEE_ID in (select EMPLOYEE_ID from employees where EMAIL = func_email);
-END $$
+delimiter $$
+create function buscar_quantidade_de_empregos_por_funcionario(func_email varchar(100))
+RETURNS INT READS SQL DATA
+begin
+DECLARE qt_func INT;
+select count(EMPLOYEE_ID) from job_history where EMPLOYEE_ID in (select EMPLOYEE_ID from employees where EMAIL = func_email)
+into qt_func;
+return qt_func;
+end $$
 delimiter ;
-call buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
+select buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
