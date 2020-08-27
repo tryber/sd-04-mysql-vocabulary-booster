b5f6f30306d020574321466a3d4adb494e33aa97
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:43:28 -0300

    Update desafio11.sql

--- desafio11.sql
+++ desafio11.sql
@@ -21,6 +21,7 @@ SELECT
   c1.ContactName AS Nome,
   c1.Country AS País,
   COUNT(c2.CustomerID) AS `Número de compatriotas`
+-- self join
 FROM w3schools.customers AS c1, w3schools.customers AS c2
 WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID
 GROUP BY c1.CustomerID
