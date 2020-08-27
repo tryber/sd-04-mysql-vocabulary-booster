c417cfb9dca8ff67a48fc4521a8b1d414e8b02fe
igorgbr <igorgphotoarte@gmail.com>
2020-08-26 22:22:04 -0300

    desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,5 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) `Nome completo`, DATE_FORMAT(JH.START_DATE, '%d/%m/%Y') `Data de início`, DATE_FORMAT(JH.END_DATE, '%d/%m/%Y') `Data de recisão`, 
+round((datediff(JH.END_DATE, JH.START_DATE) / 365), 2) `Anos trabalhados`
+FROM hr.employees E
+INNER JOIN hr.job_history JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+ORDER BY `Nome completo`, `Anos trabalhados`;
