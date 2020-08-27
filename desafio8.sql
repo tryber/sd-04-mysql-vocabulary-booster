510e476526b32f825f07ac8f44fcb2bb13f49faa
juniomelos <60393498+juniomelos@users.noreply.github.com>
2020-08-27 17:50:33 -0300

    Update desafio8.sql
    Geralmente JOIN é bem mais limpo, mas achei até legal esse outro jeito.

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,11 @@
 SELECT c.ContactName AS 'Nome de contato',
   s.ShipperName AS 'Empresa que fez o envio',
   o.OrderDate AS 'Data do pedido'
-  FROM w3schools.orders AS o
-  INNER JOIN w3schools.customers AS c
-  ON o.CustomerID = c.CustomerID
-  INNER JOIN w3schools.shippers AS s
-  ON s.ShipperID = o.ShipperID
-  WHERE o.ShipperID IN (1, 2)
-  ORDER BY c.ContactName, s.ShipperName, o.OrderDate;
+ FROM
+ w3schools.orders AS o,
+w3schools.customers AS c,
+w3schools.shippers AS s
+WHERE (c.CustomerID = o.CustomerID)
+AND (s.ShipperID = o.ShipperID)
+AND s.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
