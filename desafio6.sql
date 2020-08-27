8af48ff562d722b1526f9a35dafcaf3bba4d70c3
Julio Cesar <stininmatic@hotmail.com>
2020-08-27 01:09:43 -0300

    Ohh S Here we go again

--- desafio6.sql
+++ desafio6.sql
@@ -6,4 +6,4 @@ from hr.employees e
 inner join hr.job_history h on e.EMPLOYEE_ID = h.EMPLOYEE_ID
 inner join hr.jobs j on j.JOB_ID = h.JOB_ID /* ligando as chaves estrangeiras */
 inner join hr.departments d on d.DEPARTMENT_ID = h.DEPARTMENT_ID
-order by concat(e.first_name, ' ', e.last_name) desc, d.DEPARTMENT_NAME; 
+order by concat(e.first_name, ' ', e.last_name) desc, Cargo; 
