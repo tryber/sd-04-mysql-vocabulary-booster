c9c5a679dded7506afe8cd06603bf4637b274ad0
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-08-27 13:57:43 -0300

    desafio 15 - 02

--- desafio15.sql
+++ desafio15.sql
@@ -1,20 +1,16 @@
 USE hr;
-DELIMITER $$
-
-CREATE PROCEDURE buscar_media_por_cargo(IN cargoName VARCHAR(100))
-BEGIN
-   SELECT
-ROUND(AVG (SALARY),2)
+DELIMITER $ $ CREATE PROCEDURE buscar_media_por_cargo(IN cargoName VARCHAR(100)) BEGIN
+SELECT
+  ROUND(AVG (SALARY), 2)
 FROM
-  hr.employees 
-  WHERE JOB_ID LIKE
-  (
-  SELECT JOB_ID
-  FROM hr.jobs
-  WHERE JOB_TITLE = cargoName
-  )
-  ;
-  
-END $$
-
-DELIMITER ;
+  hr.employees
+WHERE
+  JOB_ID LIKE (
+    SELECT
+      JOB_ID
+    FROM
+      hr.jobs
+    WHERE
+      JOB_TITLE = cargoName
+  );
+END $ $ DELIMITER;
