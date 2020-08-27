4a88a585adad4be5ac36e1907931fc9c65756fb4
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:52:30 -0300

    Update desafio7.sql

--- desafio7.sql
+++ desafio7.sql
@@ -5,6 +5,9 @@ SELECT
   h.START_DATE AS `Data de início`,
   employees.SALARY AS `Salário`
 FROM hr.employees AS employees
-INNER JOIN hr.job_history AS h on employees.EMPLOYEE_ID = h.EMPLOYEE_ID
-  WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
-  ORDER BY `Nome completo`, `Data de início`;
+  JOIN hr.job_history AS h
+    ON employees.EMPLOYEE_ID = h.EMPLOYEE_ID AND MONTH(h.START_DATE) BETWEEN 1 AND 3
+-- Poderia ter sido feito da seguinte forma, porém o uso do WHERE não é realmente necessário:
+-- JOIN hr.job_history AS h on employees.EMPLOYEE_ID = h.EMPLOYEE_ID
+--   WHERE MONTH(h.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo`, `Data de início`;
