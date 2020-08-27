771e730edae63430d6eefb4cd89bff3772f2de3c
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 15:05:34 -0300

    Identação desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -7,5 +7,5 @@
 -- SELECT IF(condicao, valor_se_verdadeiro, valor_se_falso);
 SELECT
   COUNTRY_NAME AS País, IF(REGION_ID = 1, 'incluído', 'não incluído') AS "Status Inclusão"
-  FROM hr.countries
+FROM hr.countries
 ORDER BY COUNTRY_NAME;
