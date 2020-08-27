a175aed0017136192abbdebd455d6ca701e99eac
juniomelos <60393498+juniomelos@users.noreply.github.com>
2020-08-27 18:00:05 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -1,11 +1,11 @@
 SELECT c.ContactName AS 'Nome de contato',
   s.ShipperName AS 'Empresa que fez o envio',
   o.OrderDate AS 'Data do pedido'
- FROM
- w3schools.orders AS o,
+FROM
+w3schools.orders AS o,
 w3schools.customers AS c,
 w3schools.shippers AS s
 WHERE (c.CustomerID = o.CustomerID)
 AND (s.ShipperID = o.ShipperID)
-AND s.ShipperName IN ('Speedy Express', 'United Package')
+AND s.ShipperID IN (1, 2)
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
