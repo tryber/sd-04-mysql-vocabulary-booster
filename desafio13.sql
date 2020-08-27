7c6507e2a6383f47140ac39342877a07aee6949c
Lucas Yoshida <62261329+yoshidalucas92@users.noreply.github.com>
2020-08-27 16:45:06 -0300

    Update desafio13.sql

--- desafio13.sql
+++ desafio13.sql
@@ -4,6 +4,6 @@ SELECT
   p.ProductName AS Produto,
   p.Price AS Preço
 FROM w3schools.order_details AS od
-INNER JOIN w3schools.products AS p ON od.ProductID = p.ProductID
-WHERE od.Quantity > 80
+  JOIN w3schools.products AS p
+    ON od.ProductID = p.ProductID AND od.Quantity > 80
 ORDER BY Produto;
