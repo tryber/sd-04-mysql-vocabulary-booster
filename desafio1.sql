88ea39a16d66db58f03f6bc580e28c524b6260ea
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-08-28 15:09:08 -0300

    Arrumando exercicio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,4 @@
-SELECT country_name, if(region_name="Europe", "incluído", "não incluído") as "Status Inclusão"
+SELECT country_name AS "País", if(region_name="Europe", "incluído", "não incluído") as "Status Inclusão"
 FROM hr.countries as c INNER JOIN hr.regions as r
 ON c.region_id = r.region_id
 ORDER BY country_name;
