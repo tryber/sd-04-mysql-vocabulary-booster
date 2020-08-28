07b497274830ad72c4c99ac6cebde0a5cb369f48
lourenco <loulage@gmail.com>
2020-08-28 12:15:02 -0300

    fixed desafio03, added ORDER BY on CARGO

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
 SELECT JOB_TITLE as Cargo,
 (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 from hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY) ASC;
+ORDER BY (MAX_SALARY - MIN_SALARY) ASC, Cargo ASC;
