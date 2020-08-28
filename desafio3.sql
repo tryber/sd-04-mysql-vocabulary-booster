08bd82107c3ddaa393f94da47c09ae38ac435650
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-08-28 11:47:24 -0300

    Arrumando codeClimate

--- desafio3.sql
+++ desafio3.sql
@@ -5,4 +5,3 @@ SELECT * FROM hr.departments;
 SELECT job_title as Cargo, (max_salary - min_salary) as `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
-    
--- desafio6.sql
+++ desafio6.sql
@@ -4,10 +4,9 @@ SELECT
 	start_date as `Data de início do cargo`,
 	department_name as `Departamento`
 FROM hr.job_history AS jh JOIN hr.employees AS e 
-	ON jh.employee_id = e.employee_id
+ON jh.employee_id = e.employee_id
 JOIN hr.jobs AS j
-	ON	jh.job_id = j.job_id
+ON	jh.job_id = j.job_id
 JOIN hr.departments AS d
-	ON d.department_id = jh.department_id
+ON d.department_id = jh.department_id
 ORDER BY `Nome completo` DESC, `Cargo`
-	
