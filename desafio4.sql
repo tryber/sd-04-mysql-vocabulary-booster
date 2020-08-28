ce012a4e8b58ba1893d110757008b243a7599c6d
danilossousa <danilosantossousa@gmail.com>
2020-08-28 08:17:08 -0300

    desafio4

---
+++ desafio4.sql
@@ -0,0 +1,18 @@
+SELECT 
+  j.JOB_TITLE Cargo,
+  ROUND(AVG(e.SALARY), 2) `Média salarial`,
+  CASE 
+    WHEN (ROUND(AVG(e.SALARY), 2) >= 2000 AND ROUND(AVG(e.SALARY), 2) <= 5800) THEN 'Júnior'
+    WHEN (ROUND(AVG(e.SALARY), 2) >= 5801 AND ROUND(AVG(e.SALARY), 2) <= 7500) THEN 'Pleno'
+    WHEN (ROUND(AVG(e.SALARY), 2) >= 7501 AND ROUND(AVG(e.SALARY), 2) <= 10500) THEN 'Sênior'
+    ELSE 'CEO'
+  END Senioridade
+FROM
+  hr.jobs j,
+  hr.employees e
+WHERE
+  j.JOB_ID = e.JOB_ID
+GROUP BY Cargo
+ORDER BY 
+  `Média salarial`,
+  Cargo;
