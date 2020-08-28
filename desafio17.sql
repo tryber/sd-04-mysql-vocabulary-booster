d4d49c4c9797df9cfd8af13769afa45e98a34ad5
matheusysd <matheusyurisd@gmail.com>
2020-08-28 16:43:45 -0300

    Desafio 17

---
+++ desafio17.sql
@@ -0,0 +1,8 @@
+ SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS `Nome completo`,
+  DATE_FORMAT(job_history.start_date, '%d/%m/%Y') AS `Data de início`,
+  DATE_FORMAT(job_history.end_date, '%d/%m/%Y') AS `Data de rescisão`,
+  round(datediff(job_history.end_date, job_history.start_date) / 365, 2) AS `Anos trabalhados`
+FROM hr.job_history AS job_history
+  INNER JOIN hr.employees AS employees ON job_history.employee_id = employees.employee_id
+ORDER BY `Nome completo`,
+  `Anos trabalhados`;
