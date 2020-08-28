ca441c824d6d27a20ee49804773225a426d1fd64
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-08-28 11:43:01 -0300

    Arrumando codeClimate

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT country_name, if(region_name="Europe", "incluído", "não incluído") as "Status Inclusão"
-    FROM hr.countries as c INNER JOIN hr.regions as r
+FROM hr.countries as c INNER JOIN hr.regions as r
 	ON c.region_id = r.region_id
-    ORDER BY country_name;
+ORDER BY country_name;
     
--- desafio3.sql
+++ desafio3.sql
@@ -3,6 +3,6 @@ SELECT * FROM hr.employees;
 SELECT * FROM hr.departments;
 
 SELECT job_title as Cargo, (max_salary - min_salary) as `Diferença entre salários máximo e mínimo`
-	from hr.jobs
-    ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
+FROM hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
     
