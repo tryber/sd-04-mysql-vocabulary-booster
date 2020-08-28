c556811cd80736942ddb1a497b0883952c8f68fb
Orlando <programadorlando@gmail.com>
2020-08-28 13:53:47 -0300

    correcoes desafio 17

--- desafio17.sql
+++ desafio17.sql
@@ -7,7 +7,7 @@ SELECT
             employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID) AS `Nome completo`,
         DATE_FORMAT(START_DATE, '%d/%m/%Y') AS `Data de início`,
         DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
-        ROUND((END_DATE - START_DATE) / 8760, 2) AS `Anos trabalhados`
+        ROUND(DATEDIFF(END_DATE,START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM
     hr.job_history
 WHERE YEAR(START_DATE) <> 0
