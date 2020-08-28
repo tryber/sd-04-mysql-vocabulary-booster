d2babfb61ec036ebee7d3e102d32e20ca00abd85
vinicius-vasconcelos <viniciussouzav@gmail.com>
2020-08-28 15:13:54 -0300

    Arrumando exercicio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,3 @@
-SELECT * FROM hr.jobs;
-SELECT * FROM hr.employees;
-SELECT * FROM hr.departments;
-
 SELECT job_title as Cargo, (max_salary - min_salary) as `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
 ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
