1c3d57819c58057e6816a159983db4b2abc71e90
danilossousa <danilosantossousa@gmail.com>
2020-08-28 06:45:42 -0300

    desafio3

---
+++ desafio3.sql
@@ -0,0 +1,6 @@
+SELECT 
+    JOB_TITLE Cargo,
+    (MAX_SALARY - MIN_SALARY) `Diferença entre salários máximo e mínimo`
+FROM
+    hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
