6bebfd57bbfd9d297d78c00c460a102ca63b0b71
danilossousa <danilosantossousa@gmail.com>
2020-08-29 07:02:17 -0300

    desafio13

---
+++ desafio13.sql
@@ -0,0 +1,11 @@
+SELECT
+  p.ProductName `Produto`,
+  p.Price `Preço`
+FROM
+  w3schools.products p
+INNER JOIN
+  w3schools.order_details od ON
+  p.ProductID = od.ProductID AND
+  od.Quantity > 80
+ORDER BY
+  `Produto`;
