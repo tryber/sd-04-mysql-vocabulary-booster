c19bd22b3f42a184d0f8face552da08285285d9c
danilossousa <danilosantossousa@gmail.com>
2020-08-29 05:58:28 -0300

    desafio11 t3

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,15 @@
 SELECT 
   c.ContactName Nome,
   c.Country País,
-  COUNT(c.Country) - 1 `Número de compatriotas`
+  COUNT(*) - 1 `Número de compatriotas`
 FROM
   w3schools.customers c,
   w3schools.customers c2
 WHERE
   c.Country = c2.Country
 GROUP BY
-  Nome
+  Nome,
+  País
 HAVING
   `Número de compatriotas` > 0
 ORDER BY
