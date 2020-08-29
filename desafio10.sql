5a0d4376cc9a0a4fc800edba51a60d2b3b8f900e
danilossousa <danilosantossousa@gmail.com>
2020-08-29 02:14:20 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,17 @@
+SELECT
+  p.ProductName Produto,
+  MIN(od.Quantity) Mínima,
+  MAX(od.Quantity) Máxima,
+  ROUND(AVG(od.Quantity), 2) Média
+FROM
+  w3schools.products p
+INNER JOIN
+  w3schools.order_details od ON
+  p.ProductID = od.ProductID
+GROUP BY
+  Produto
+HAVING
+  Média > 20.00
+ORDER BY
+  Média,
+  Produto;
