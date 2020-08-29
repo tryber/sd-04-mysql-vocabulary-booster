e51176961a7b7f33603096b0e761505ed18bf75f
danilossousa <danilosantossousa@gmail.com>
2020-08-29 05:38:06 -0300

    desafio11 t2

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
 SELECT 
   c.ContactName Nome,
   c.Country País,
-  COUNT(*) - 1 `Número de compatriotas`
+  COUNT(c.Country) - 1 `Número de compatriotas`
 FROM
   w3schools.customers c,
   w3schools.customers c2
