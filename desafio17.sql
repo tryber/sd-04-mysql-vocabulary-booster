b30b0653309dfc72f0344eb2624ba10cedca8087
procopio420 <lucas-procopio@outlook.com>
2020-08-29 01:59:20 -0300

    more ignores

--- desafio17.sql
+++ desafio17.sql
@@ -1,5 +1,4 @@
-SELECT
-	CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
+SELECT CONCAT(em.FIRST_NAME, ' ', em.LAST_NAME) AS 'Nome completo',
     DATE_FORMAT(jh.START_DATE ,'%d/%m/%Y') AS 'Data de início',
     DATE_FORMAT(jh.END_DATE ,'%d/%m/%Y') AS 'Data de rescisão',
     ROUND(DATEDIFF(jh.END_DATE,jh.START_DATE) / 365, 2) AS 'Anos trabalhados'
