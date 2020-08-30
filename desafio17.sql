a70230d3832c68ea44b27bc6b750bfb3fcabd18e
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-08-30 16:37:03 -0300

    17

---
+++ desafio17.sql
@@ -0,0 +1,27 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION data_brasileira(data_gringa DATE)
+RETURNS varchar(50) READS SQL DATA
+BEGIN
+RETURN 
+	(CASE
+	WHEN DAY(data_gringa) < 10 and MONTH(data_gringa) < 10 THEN CONCAT(0, DAY(data_gringa), '/', 0,  MONTH(data_gringa), '/', YEAR(data_gringa))
+	WHEN DAY(data_gringa) < 10 THEN CONCAT(0, DAY(data_gringa), '/', MONTH(data_gringa), '/', YEAR(data_gringa))
+    WHEN MONTH(data_gringa) < 10 THEN CONCAT(DAY(data_gringa), '/', 0, MONTH(data_gringa), '/', YEAR(data_gringa))
+    ELSE CONCAT(DAY(data_gringa), '/', MONTH(data_gringa), '/', YEAR(data_gringa))
+	END);
+END $$
+
+DELIMITER ;
+# usando uma função e data format pra converter datas
+SELECT 
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+    (DATE_FORMAT(START_DATE, '%d/%m/%Y')) AS 'Data de início',
+    (SELECT DATA_BRASILEIRA(END_DATE)) AS 'Data de rescisão',
+    ROUND(DATEDIFF(END_DATE,START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM
+    job_history jh
+        JOIN
+    employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+ORDER BY `Nome completo` , `Anos trabalhados`;
