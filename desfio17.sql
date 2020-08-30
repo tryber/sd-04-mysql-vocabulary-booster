962d8084d5b2c9b574b2eb4172a2c1e2aadf1081
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-08-29 22:26:12 -0300

    desafio17

--- desfio17.sql
+++ desfio17.sql
@@ -2,7 +2,9 @@ SELECT
     CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
     DATE_FORMAT(H.START_DATE, '%d/%m/%Y') AS 'Data de início',
     DATE_FORMAT(H.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
-    ROUND(DATEDIFF(H.END_DATE,H.START_DATE)/365,2) AS 'Anos trabalhados'
+    ROUND(DATEDIFF(H.END_DATE, H.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM
-    hr.employees AS E INNER JOIN hr.job_history AS H ON E.EMPLOYEE_ID = H.EMPLOYEE_ID
-    ORDER BY `Nome completo`, `Anos trabalhados`;
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS H ON E.employee_id = H.employee_id
+ORDER BY `Nome completo` , `Anos trabalhados`;
