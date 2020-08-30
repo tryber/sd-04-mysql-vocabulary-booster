b6e16b305d15e9ad8047965d1ba7ef7855d029e7
wistyk <leonardoxw@gmail.com>
2020-08-30 18:32:40 -0300

    corrigido o nome do contato

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
 SELECT (
-SELECT CustomerName FROM w3schools.customers
+SELECT ContactName FROM w3schools.customers
 WHERE CustomerID = w3schools.orders.CustomerID) AS 'Nome de contato',
 (SELECT ShipperName FROM w3schools.shippers
 WHERE ShipperID = w3schools.orders.ShipperID) AS 'Empresa que fez o envio',
