206ea385978b025fc47adac6fc4e36b75d0da4ec
julianafatsil <julianafatsil@gmail.com>
2020-08-30 16:49:11 -0300

    Remove left Join and Group By

--- desafio5.sql
+++ desafio5.sql
@@ -4,8 +4,5 @@ SELECT
     ROUND(J.MIN_SALARY / 12, 2) `Média mínima mensal`,
     ROUND(J.MAX_SALARY / 12, 2) `Média máxima mensal`
 FROM
-    hr.employees AS E
-        LEFT JOIN
-    hr.jobs AS J ON E.JOB_ID = J.JOB_ID
-GROUP BY J.JOB_TITLE
+    hr.jobs AS J
 ORDER BY `Variação Salarial` , J.JOB_TITLE;
