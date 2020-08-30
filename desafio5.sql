cc246d9c81a5e0a1d4c513b2a4dc3414914ec0a0
wistyk <leonardoxw@gmail.com>
2020-08-30 12:16:43 -0300

    retirado o group by

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,4 @@ MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
 ROUND((MIN_SALARY / 12), 2) AS 'Média mínima mensal',
 ROUND((MAX_SALARY)/ 12) AS 'Média máxima mensal'
 FROM hr.jobs
-GROUP BY JOB_ID
 ORDER BY 2, 1;
