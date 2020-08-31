cf23281c9c30d5f6d5c7b2988e95438e446928a4
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-08-31 15:57:14 -0300

    CC

--- desafio19.sql
+++ desafio19.sql
@@ -8,8 +8,7 @@ SELECT
     CONCAT(employees.FIRST_NAME,' ',employees.LAST_NAME) AS 'Nome completo',
     departments.DEPARTMENT_NAME AS 'Departamento',
     jobs.JOB_TITLE AS 'Cargo'
-FROM
-	hr.job_history AS history
+FROM hr.job_history AS history
 INNER JOIN hr.employees AS employees ON history.EMPLOYEE_ID = employees.EMPLOYEE_ID
 INNER JOIN hr.departments AS departments ON history.DEPARTMENT_ID = departments.DEPARTMENT_ID
 INNER JOIN hr.jobs AS jobs ON history.JOB_ID = jobs.JOB_ID
