9d86e03d6d151a3857df759c2b82a91d708ecdda
procopio420 <lucas-procopio@outlook.com>
2020-08-30 23:52:31 -0300

    I just wanna tell you how I'm feeling

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,7 @@
 SELECT
-	-- ContactName AS Nome,
-  Country AS País,
-  COUNT(*) - 1 AS 'Número de compatriotas' 
-FROM w3schools.customers
-GROUP BY Country
+  c1.ContactName AS Nome,
+  c1.Country AS País,
+  (SELECT COUNT(*) FROM w3schools.customers AS c2 WHERE c2.Country = c1.Country) - 1 AS 'Número de compatriotas'
+FROM w3schools.customers AS c1
 HAVING `Número de compatriotas` > 0
--- ORDER BY Nome;
+ORDER BY Nome;
