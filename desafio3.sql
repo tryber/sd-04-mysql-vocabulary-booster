cdb99c5823a518c6b60d0bc02c9b8f4d854f6305
gabriellukke <gabriellukke@outlook.com>
2020-08-31 09:46:03 -0300

    Desafio3 fix

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,4 @@
 SELECT JOB_TITLE AS Cargo,
 MIN_SALARY - MAX_SALARY AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
-ORDER BY 2, JOB_TITLE;
+ORDER BY `Diferença entre salários máximo e mínimo`, JOB_TITLE;
