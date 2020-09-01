fe209b0be509a89da90e2b602deb90f132d96d7c
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 12:48:01 -0300

    try to correct chall 18 again

--- desafio18.sql
+++ desafio18.sql
@@ -2,20 +2,20 @@ DROP FUNCTION IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
 
 DELIMITER $$
 
--- create FUNCTION
-CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(startMonth INT, startYear INT)
-RETURNS INT
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(start_month INT, start_year INT)
+
+RETURNS INT READS SQL DATA
+
 BEGIN
-    DECLARE hired INT;
+    DECLARE hired INTEGER;
 
     SELECT COUNT(e.employee_id) INTO hired
-    FROM employees AS e
-    WHERE MONTH(e.hire_date) = startMonth AND YEAR(e.hire_date) = startYear;
+    FROM hr.employees AS e
+    WHERE MONTH(e.hire_date) = start_month AND YEAR(e.hire_date) = start_year;
 
     RETURN hired;
 END $$
 
 DELIMITER ;
 
--- execute FUNCTION
-SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987) AS 'total';
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987) as 'total';
