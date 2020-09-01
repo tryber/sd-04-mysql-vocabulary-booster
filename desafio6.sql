3ab7e999ccdd88f280d4f6c4a3985e6bbe234012
Caio Magalhães <caio.lmf@gmail.com>
2020-08-31 22:51:30 -0300

    do challenge 06

---
+++ desafio6.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT(e.first_name, ' ', e.last_name) as 'Nome completo',
+    j.job_title as 'Cargo',
+    e.hire_date as 'Data de início do cargo',
+    d.department_name as 'Departamento'
+FROM employees e
+    INNER JOIN jobs j ON e.job_id = j.job_id
+    INNER JOIN departments d on e.department_id = d.department_id
+ORDER BY `Nome completo` DESC,
+    `Cargo` ASC;
