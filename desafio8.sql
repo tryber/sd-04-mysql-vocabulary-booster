fdafb5ab93be4af45c7fd72b1fe4df36ca7dec88
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 01:49:27 -0300

    cnahge customer name to contactname on challenge 08

--- desafio8.sql
+++ desafio8.sql
@@ -1,10 +1,10 @@
-SELECT c.customername AS 'Nome de contato',
+SELECT c.contactname AS 'Nome de contato',
     s.shippername AS 'Empresa que fez o envio',
     o.orderdate AS 'Data do pedido'
-FROM orders as o
-    INNER JOIN shippers s on o.shipperid = s.shipperid
-    INNER JOIN customers c on o.customerid = c.customerid
+FROM customers as c
+    JOIN orders AS o on c.customerid = o.customerid
+    JOIN shippers AS s on o.shipperid = s.shipperid
 WHERE s.shippername in ('Speedy Express', 'United Package')
-ORDER BY c.customername ASC,
+ORDER BY c.contactname ASC,
     s.shippername ASC,
     o.orderdate ASC;
