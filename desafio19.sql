58e69fac03f05ee58cf0e493ea3aeb0a59b4bfa5
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 12:15:34 -0300

    cc challenge 19

--- desafio19.sql
+++ desafio19.sql
@@ -5,12 +5,12 @@ DELIMITER $$
 CREATE PROCEDURE exibir_historico_completo_por_funcionario (IN email VARCHAR(150))
 BEGIN
     SELECT CONCAT(e.first_name, ' ', e.last_name) as 'Nome completo',
-	j.job_title AS 'Cargo',
-    d.department_name AS 'Departamento'
+        j.job_title AS 'Cargo',
+        d.department_name AS 'Departamento'
     FROM employees AS e
-    INNER JOIN job_history AS jh ON e.employee_id = jh.employee_id
-    INNER JOIN jobs AS j ON jh.job_id = j.job_id
-    INNER JOIN departments AS d ON jh.department_id = d.department_id
+        INNER JOIN job_history AS jh ON e.employee_id = jh.employee_id
+        INNER JOIN jobs AS j ON jh.job_id = j.job_id
+        INNER JOIN departments AS d ON jh.department_id = d.department_id
     WHERE e.email = email;
 END $$
 
