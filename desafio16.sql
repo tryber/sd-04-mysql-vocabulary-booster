c1d6d35dc2111ed41133aef95c4aaa66efacf981
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 11:13:34 -0300

    correct cahllenge 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,15 +1,20 @@
-DROP PROCEDURE IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
+DROP FUNCTION IF EXISTS buscar_quantidade_de_empregos_por_funcionario;
 
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario (IN email VARCHAR(150))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(150))
+RETURNS INT
 BEGIN
-    SELECT COUNT(e.job_id) AS 'total_empregos'
+    DECLARE total_empregos INT;
+
+    SELECT COUNT(e.job_id) INTO total_empregos
     FROM employees AS e
     INNER JOIN job_history AS jh on e.employee_id = jh.employee_id
     WHERE e.email = email;
+
+    RETURN total_empregos;
 END $$
 
 DELIMITER ;
 
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') AS 'total_empregos';
