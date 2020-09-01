2a5f8afe4d9ea5477e6de3c005d8681aea648031
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 10:46:09 -0300

    do challenge 15

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+DROP PROCEDURE IF EXISTS buscar_media_por_cargo;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo (IN cargo VARCHAR(150))
+BEGIN
+    SELECT ROUND(AVG(e.salary)) AS 'Média salarial'
+    FROM jobs AS j
+    INNER JOIN employees AS e on e.job_id = j.job_id
+    WHERE j.job_title = cargo;
+END $$
+
+DELIMITER ;
+
+-- Execute PROCEDURE
+
+CALL buscar_media_por_cargo('Programmer');
