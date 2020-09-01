a07dfbd925528083d26ea4e27cec62d29dde4515
Caio Magalhães <caio.lmf@gmail.com>
2020-09-01 11:34:49 -0300

    correct challenge 17 years calc to wrong way

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
     DATE_FORMAT(jh.start_date, '%d/%m/%Y') AS 'Data de início',
     DATE_FORMAT(jh.end_date, '%d/%m/%Y') AS 'Data de rescisão',
-    TIMESTAMPDIFF(YEAR, '2011/08/25', '2017/08/25') AS 'Anos trabalhados'
+    ROUND(DATEDIFF(jh.end_date, jh.start_date) / 365, 2) AS 'Anos trabalhados'
 FROM job_history AS jh
     INNER JOIN employees AS e ON jh.employee_id = e.employee_id
 ORDER BY `Nome completo` ASC,
