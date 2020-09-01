32d6ee89feb8b01809611a3b28f9c50e9c68e451
Caio Magalhães <caio.lmf@gmail.com>
2020-08-31 23:15:18 -0300

    Add month condition to challenge 07

--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,5 @@ SELECT UPPER(CONCAT(e.first_name, ' ', e.last_name)) as 'Nome completo',
     e.salary as 'Salário'
 FROM employees e
     INNER JOIN job_history jh ON e.employee_id = jh.employee_id
-WHERE MONTH(jh.start_date) IN (1, 2, 3)
 ORDER BY `Nome completo` ASC,
     `Data de início` ASC;
