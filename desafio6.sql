4b2cb36e2b68471282c5737150430fbf1da44787
gabriellukke <gabriellukke@outlook.com>
2020-09-01 11:51:37 -0300

    Some Adjustments

--- desafio6.sql
+++ desafio6.sql
@@ -1,12 +1,12 @@
-SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS 'Nome completo',
-jobs.JOB_TITLE AS 'Cargo',
-history.START_DATE AS 'Data de início no cargo',
+SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
+jobs.JOB_TITLE AS `Cargo`,
+job_history.START_DATE AS 'Data de início no cargo',
 departments.DEPARTMENT_NAME AS 'Departamento'
-FROM hr.employees AS employees
+FROM hr.job_history AS job_history
 JOIN hr.jobs AS jobs
-ON employees.JOB_ID = jobs.JOB_ID
-JOIN hr.job_history AS history
-ON employees.EMPLOYEE_ID = history.EMPLOYEE_ID
+ON job_history.JOB_ID = jobs.JOB_ID
+JOIN hr.employees AS employees
+ON job_history.EMPLOYEE_ID = employees.EMPLOYEE_ID
 JOIN hr.departments AS departments
-ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
-ORDER BY 'Nome completo' DESC, 'Cargo' DESC;
+ON job_history.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, `Cargo`;
--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
-SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome completo',
-history.START_DATE AS 'Data de início no cargo',
+SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
+history.START_DATE AS `Data de início`,
 employees.SALARY AS 'Salário'
 FROM hr.employees AS employees
 JOIN hr.job_history AS history
 ON employees.EMPLOYEE_ID = history.EMPLOYEE_ID
 WHERE MONTH(history.START_DATE) IN (1, 2, 3)
-ORDER BY 'Nome completo', 'Data de início';
+ORDER BY `Nome completo`, `Data de início`;
