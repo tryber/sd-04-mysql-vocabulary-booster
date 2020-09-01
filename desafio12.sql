4c9540e7bc54aee2795298ce917f93600030b9b8
wistyk <leonardoxw@gmail.com>
2020-09-01 07:34:42 -0300

    detalhes, sempre os detalhes

--- desafio12.sql
+++ desafio12.sql
@@ -3,7 +3,7 @@ sameTableOne.SALARY AS 'Salário funcionário 1',
 sameTableOne.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(sameTableTwo.FIRST_NAME, ' ', sameTableTwo.LAST_NAME) AS `Nome completo funcionário 2`,
 sameTableTwo.SALARY AS 'Salário funcionário 2',
-sameTableTwo.PHONE_NUMBER AS 'Telefone funcionário 1'
+sameTableTwo.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS sameTableOne, hr.employees AS sameTableTwo
 WHERE sameTableOne.JOB_ID = sameTableTwo.JOB_ID
 AND CONCAT(sameTableOne.FIRST_NAME, ' ', sameTableOne.LAST_NAME) <>
