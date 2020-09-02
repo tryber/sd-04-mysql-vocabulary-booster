0a3aff13aef40c45b96cb95d2ea659dda076b102
djonata-dias <diasdjonata@gmail.com>
2020-09-02 19:22:03 -0300

    Desafio 10 teste 2

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,8 @@
 SELECT
     p.ProductName AS 'Produto',
-    MIN(od.Quantity) AS 'quantidade mínima',
-    MIN(od.Quantity) AS 'quantidade máxima',
-    AVG(od.Quantity) AS 'média de quantidade nos pedidos'
+    MIN(od.Quantity) AS 'Mínima',
+    MIN(od.Quantity) AS 'Máxima',
+    AVG(od.Quantity) AS 'Média'
 FROM
     w3schools.products AS p
     INNER JOIN w3schools.order_details AS od ON od.ProductID = p.ProductID
