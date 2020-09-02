434b0d4c823a1b768415e235a34e400181a7feb0
djonata-dias <diasdjonata@gmail.com>
2020-09-02 19:46:54 -0300

    Desafio 10 teste 3

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,15 @@
 SELECT
     p.ProductName AS 'Produto',
     MIN(od.Quantity) AS 'Mínima',
-    MIN(od.Quantity) AS 'Máxima',
-    AVG(od.Quantity) AS 'Média'
+    MAX(od.Quantity) AS 'Máxima',
+    ROUND(AVG(od.Quantity), 2) AS Média
 FROM
     w3schools.products AS p
     INNER JOIN w3schools.order_details AS od ON od.ProductID = p.ProductID
 GROUP BY
     p.ProductID
+HAVING
+    Média > 20.00
 ORDER BY
-    AVG(od.Quantity),
+    ROUND(AVG(od.Quantity), 2),
     p.ProductName;
