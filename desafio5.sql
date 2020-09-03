19a6f0839a76dbcf4ad09e1345a9607f640c95fd
Victor L <victor.lustosacp@gmail.com>
2020-09-03 16:57:20 -0300

    CC

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
-SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS `Diferença entre salários máximo e mínimo`,
+SELECT JOB_TITLE AS Cargo, (MAX_SALARY - MIN_SALARY) AS `Variação Salarial`,
 ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`, ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
 FROM hr.jobs
-ORDER BY `Diferença entre salários máximo e mínimo`, Cargo;
+ORDER BY `Variação Salarial`, Cargo;
