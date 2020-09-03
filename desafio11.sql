60c98734838dae41c125ee0ec4d1225282271f3e
djonata-dias <diasdjonata@gmail.com>
2020-09-02 21:26:11 -0300

    Desafio 11 Teste 1

---
+++ desafio11.sql
@@ -0,0 +1,20 @@
+SELECT
+    A.ContactName AS Nome,
+    (
+        SELECT
+            Country
+        FROM
+            w3schools.customers
+        WHERE
+            ContactName = A.ContactName
+    ) AS País,
+    COUNT(B.ContactName) AS 'Número de compatriotas'
+FROM
+    w3schools.customers A,
+    w3schools.customers B
+WHERE
+    A.Country = B.Country
+GROUP BY
+    A.ContactName
+ORDER BY
+    A.ContactName;
