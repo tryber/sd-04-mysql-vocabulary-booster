c5ee2491ececa459391afebc1a4a39187c6cdecd
Victor L <victor.lustosacp@gmail.com>
2020-09-04 14:45:56 -0300

    6

--- desafio6.sql
+++ desafio6.sql
@@ -3,5 +3,5 @@ j.JOB_TITLE AS Cargo,
 jh.START_DATE AS `Data de início do cargo`,
 d.DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees AS e, hr.jobs AS j, hr.job_history AS jh, hr.departments AS d
-where e.JOB_ID = j.JOB_ID AND e.EMPLOYEE_ID = jh.EMPLOYEE_ID and jh.DEPARTMENT_ID = d.DEPARTMENT_ID
+where j.JOB_ID = jh.JOB_ID AND e.EMPLOYEE_ID = jh.EMPLOYEE_ID and jh.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo  ;
