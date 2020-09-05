ae0dfad5c61c379d07d4bb9a5454714813ea9223
saturnian99 <saturnian.dsg@gmail.com>
2020-09-05 19:41:14 -0300

    Fixing Desafio 1/6

--- desafio6.sql
+++ desafio6.sql
@@ -3,17 +3,17 @@ SELECT
 SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME)
 FROM hr.employees
 WHERE EMPLOYEE_ID = jh.EMPLOYEE_ID
-) AS Nome_Completo,
+) AS `Nome completo`,
 (
 SELECT JOB_TITLE
 FROM hr.jobs
 WHERE JOB_ID = jh.JOB_ID
-) AS Cargo,
-START_DATE AS 'Data de início de cargo',
+) AS `Cargo`,
+START_DATE AS `Data de início de cargo`,
 (
 SELECT DEPARTMENT_NAME
 FROM hr.departments
 WHERE DEPARTMENT_ID = jh.DEPARTMENT_ID
-) AS 'Departamento'
+) AS `Departamento`
 FROM hr.job_history AS jh
-ORDER BY Nome_Completo, Cargo;
+ORDER BY `Nome completo`, `Cargo`;
