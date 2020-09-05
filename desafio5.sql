d45621a408cd083107337242a41a9431a5254ba4
gabriellukke <gabriellukke@outlook.com>
2020-09-05 14:18:09 -0300

    Desafio5 fix v1.0

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE AS 'Cargo',
-MAX_SALARY - MIN_SALARY AS 'Variação Salarial',
+(MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
 ROUND(min_salary / 12, 2) AS 'Média mínima mensal',
 ROUND(max_salary / 12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY `Variação Salarial`, 'Cargo';
+ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
