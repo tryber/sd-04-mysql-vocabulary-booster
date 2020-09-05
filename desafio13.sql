5b0eeada7499ed72eaf7a28c39e86551c81d9bbd
gabriellukke <gabriellukke@outlook.com>
2020-09-05 17:35:04 -0300

    Desafio13 Fix v1.0

--- desafio13.sql
+++ desafio13.sql
@@ -2,5 +2,5 @@ SELECT products.ProductName AS 'Produto',
 products.Price AS 'Preço'
 FROM w3schools.products AS products
 JOIN w3schools.order_details AS details
-ON products.Price > 80 AND products.ProductID = details.ProductID
+ON details.Quantity > 80 AND products.ProductID = details.ProductID
 ORDER BY products.ProductName;
