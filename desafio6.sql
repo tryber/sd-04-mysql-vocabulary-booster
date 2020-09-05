b83789c9cd2c859c10f72341a063a5c6db97f3ae
gabriellukke <gabriellukke@outlook.com>
2020-09-05 14:25:36 -0300

    Desafio6 fix v1.0

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
 jobs.JOB_TITLE AS `Cargo`,
-job_history.START_DATE AS `Data de início no cargo`,
-departments.DEPARTMENT_NAME AS 'Departamento'
+job_history.START_DATE AS `Data de início do cargo`,
+departments.DEPARTMENT_NAME AS `Departamento`
 FROM hr.job_history AS job_history
 JOIN hr.jobs AS jobs
 ON job_history.JOB_ID = jobs.JOB_ID
