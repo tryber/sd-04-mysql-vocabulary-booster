17a18d891f986c73f33cea1733ebe495cdcf9bff
gabriellukke <gabriellukke@outlook.com>
2020-09-05 15:01:28 -0300

    Desafio8 fix v2.0

--- desafio8.sql
+++ desafio8.sql
@@ -2,10 +2,26 @@ SELECT customers.ContactName AS `Nome de contato`,
 shippers.ShipperName AS `Empresa que fez o envio`,
 orders.OrderDate AS `Data do pedido`
 FROM w3schools.orders AS orders
-JOIN w3schools.customers AS customers
-ON orders.customerID = customers.customerID
 JOIN w3schools.shippers AS shippers
-ON orders.shipperID = orders.shipperID
--- WHERE shippers.ShipperName IN ('Speedy Express' , 'United Package')
-AND (shippers.ShipperName = 'Speedy Express' OR shippers.ShipperName = 'United Package')
-ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
+ON orders.ShipperID = shippers.ShipperID
+JOIN w3schools.customers AS customers
+ON orders.CustomerID = customers.CustomerID
+WHERE shippers.ShipperName IN ('Speedy Express' , 'United Package')
+ORDER BY customers.ContactName, shippers.ShipperName, orders.OrderDate;
+
+/*SELECT 
+shippers.ShipperName AS `Empresa que fez o envio`,
+orders.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS orders
+JOIN w3schools.shippers AS shippers
+ON orders.ShipperID = shippers.ShipperID;
+
+SELECT  customers.ContactName AS `Nome de contato`,
+orders.OrderDate AS `Data do pedido`
+FROM w3schools.orders AS orders
+JOIN w3schools.customers AS customers
+ON orders.CustomerID = customers.CustomerID;*/
+
+
+
+
