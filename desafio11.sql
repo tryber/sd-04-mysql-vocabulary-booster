13ef3be0cde8f0182580f259d5ccc7c3e2cd8faf
gabriellukke <gabriellukke@outlook.com>
2020-09-05 16:13:20 -0300

    Desafio11 Fix v2.0

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
-SELECT c1.CustomerName AS 'Nome',
+SELECT c1.ContactName AS 'Nome',
 c1.Country AS `País`,
 COUNT(c2.Country) AS `Número de compatriotas`
 FROM w3schools.customers AS c1
 JOIN w3schools.customers AS c2
 ON c1.CustomerID <> c2.CustomerID AND c1.Country = c2.Country
 GROUP BY c1.CustomerID
-ORDER BY c1.CustomerName;
+ORDER BY c1.ContactName;
