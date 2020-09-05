16dc8ac273703a0959ff23a308f435f71e82956a
gabriellukke <gabriellukke@outlook.com>
2020-09-05 15:59:33 -0300

    Desafio11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT c1.CustomerName AS 'Nome',
+c1.Country AS `País`,
+COUNT(c2.Country) AS `Número de compatriotas`
+FROM w3schools.customers AS c1
+JOIN w3schools.customers AS c2
+ON c1.CustomerID <> c2.CustomerID AND c1.Country = c2.Country
+GROUP BY c1.CustomerID;
