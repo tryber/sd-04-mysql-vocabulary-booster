534647f111a070a3e6c8e90ebd23eb675f1e581d
djonata-dias <diasdjonata@gmail.com>
2020-09-07 20:34:01 -0300

    Desafio 17 Teste 3

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,19 @@
 SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
-    DATE(jh.START_DATE) 'Data de início',
-    jh.END_DATE 'Data de rescisão',
+    CONCAT(
+        DAY(jh.START_DATE),
+        '/',
+        MONTH(jh.START_DATE),
+        '/',
+        YEAR(jh.START_DATE)
+    ) 'Data de início',
+    CONCAT(
+        DAY(jh.END_DATE),
+        '/',
+        MONTH(jh.END_DATE),
+        '/',
+        YEAR(jh.END_DATE)
+    ) 'Data de rescisão',
     ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) 'Anos trabalhados'
 FROM
     hr.job_history jh
@@ -9,3 +21,8 @@ FROM
 ORDER BY
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ASC,
     (YEAR(jh.END_DATE) - YEAR(jh.START_DATE)) ASC;
+
+SELECT
+    *
+FROM
+    hr.job_history;
