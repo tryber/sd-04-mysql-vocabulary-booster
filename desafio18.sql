fa67c0ea7b0227e2e805a47887fbd1ab50abec8b
gabriellukke <gabriellukke@outlook.com>
2020-09-06 21:51:07 -0300

    Desafio18 v1.0

--- desafio18.sql
+++ desafio18.sql
@@ -5,9 +5,9 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(yr INT, mth 
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total INT;
-SELECT COUNT(EMPLOYEE_ID)
+SELECT COUNT(EMPLOYEE_ID) INTO total
 FROM hr.employees
-WHERE (YEAR(HIRE_DATE) = yr) AND (MONTH(HIRE_DATE) = mth) INTO total;
+WHERE (YEAR(HIRE_DATE) = yr) AND (MONTH(HIRE_DATE) = mth);
 RETURN total;
 END $$
 DELIMITER ;
