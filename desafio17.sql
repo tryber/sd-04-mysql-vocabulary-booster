7904217e38327de599568c5612882557da199bca
djonata-dias <diasdjonata@gmail.com>
2020-09-07 20:30:41 -0300

    Desafio 17 Teste 2

--- desafio17.sql
+++ desafio17.sql
@@ -1,8 +1,8 @@
 SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
-    jh.START_DATE 'Data de início',
+    DATE(jh.START_DATE) 'Data de início',
     jh.END_DATE 'Data de rescisão',
-    (YEAR(jh.END_DATE) - YEAR(jh.START_DATE)) 'Anos trabalhados'
+    ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) 'Anos trabalhados'
 FROM
     hr.job_history jh
     INNER JOIN hr.employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
