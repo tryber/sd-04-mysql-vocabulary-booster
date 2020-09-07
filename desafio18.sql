4909d91a38701d2b9301f76bb0bb350a9774ab2c
gabriellukke <gabriellukke@outlook.com>
2020-09-06 21:26:32 -0300

    Desafio18

---
+++ desafio18.sql
@@ -0,0 +1,15 @@
+USE hr;
+DELIMITER $$
+DROP FUNCTION IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(yr INT, mth INT)
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total INT;
+SELECT COUNT(EMPLOYEE_ID)
+FROM hr.employees
+WHERE (YEAR(HIRE_DATE) = yr) AND (MONTH(HIRE_DATE) = mth) INTO total;
+RETURN total;
+END $$
+DELIMITER ;
+
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(1987, 6);
