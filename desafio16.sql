65b986dc380ac90facbec8410b5464800bc57338
djonata-dias <diasdjonata@gmail.com>
2020-09-07 19:55:01 -0300

    Desafio 16 Teste 3

--- desafio16.sql
+++ desafio16.sql
@@ -1,12 +1,15 @@
 USE hr;
 
-DELIMITER $$ CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(100)) BEGIN
+DELIMITER $ $ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100)) RETURNS INT READS SQL DATA BEGIN DECLARE historico INT;
+
 SELECT
-    COUNT(*)
+    COUNT(*) INTO historico
 FROM
     hr.employees e
     INNER JOIN hr.job_history jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 WHERE
     e.EMAIL LIKE (email);
 
-END $$ DELIMITER ;
+RETURN historico;
+
+END $ $ DELIMITER;
