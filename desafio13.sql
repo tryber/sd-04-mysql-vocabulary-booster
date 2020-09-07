3f0d389c0173131d7cd843cc6d7793283844cb59
fogo5000 <lucaslucas2008@gmail.com>
2020-09-07 20:35:57 -0300

    Requisition 13 Done

---
+++ desafio13.sql
@@ -0,0 +1,9 @@
+SELECT 
+    prod.ProductName AS 'Produto', prod.Price AS 'Preço'
+FROM
+    w3schools.products AS prod
+        INNER JOIN
+    w3schools.order_details AS det ON prod.ProductID = det.ProductID
+WHERE
+    det.Quantity > 80
+ORDER BY Produto;
