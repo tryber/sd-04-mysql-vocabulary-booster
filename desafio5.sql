2f5e7aae1d020878aa304690f6471ee136fe57af
fogo5000 <lucaslucas2008@gmail.com>
2020-09-07 14:35:17 -0300

    Requisition 1-5 Done

---
+++ desafio5.sql
@@ -0,0 +1,7 @@
+SELECT 
+    job.JOB_TITLE AS 'Cargo', job.MAX_SALARY - job.MIN_SALARY AS `Variação Salarial`,
+    ROUND(job.MIN_SALARY/12, 2) AS "Média mínima mensal",
+    ROUND(job.MAX_SALARY/12, 2) AS "Média máxima mensal"
+FROM
+hr.jobs AS job
+ORDER BY `Variação Salarial` ASC, Cargo;
