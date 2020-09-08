93fa46fc3e2391605223ba639292a9b649477952
felipedoss <felipecaetano25.1996@gmail.com>
2020-09-08 12:37:16 -0300

    add

--- desafio17.sql
+++ desafio17.sql
@@ -1,9 +1,8 @@
-select(select concat(first_name, ' ', last_name)from hr.employees as e
-where e.employee_id = jh.employee_id
-) as 'nome completo',
-date_format (start_date,'%d/%m/%y') as `data de início`,
-date_format (end_date,'%d/%m/%y') as `data de rescisão`,
-round(datediff(end_date, start_date) / 365, 2) as `anos trabalhados`
-from hr.job_history as jh
-where jh.employee_id <> 0
-order by `nome completo` asc, `anos trabalhados` asc;
+select concat(e.first_name, ' ', e.last_name) as `nome completo`,
+  date_format(jh.start_date, "%d/%m/%y") as 'data de início',
+  date_format(jh.end_date, "%d/%m/%y") as 'data de rescisão',
+  round(datediff(jh.end_date, jh.start_date) / 365, 2) as `anos trabalhados`
+  from hr.job_history as jh
+  inner join hr.employees as e
+  on jh.employee_id = e.employee_id
+  order by `nome completo`, `anos trabalhados`;
---
+++ desafio19.sql
@@ -0,0 +1,12 @@
+select * from employees; select * from departments; select * from job_history; select * from jobs;
+create procedure exibir_historico_completo_por_funcionario(in email_emp varchar(300))
+begin
+select concat(e.first_name, ' ', e.last_name) `nome completo`, d.department_name departamento, j.job_title cargo
+from hr.employees e 
+inner join hr.job_history jh on jh.employee_id = e.employee_id
+inner join hr.jobs j on jh.job_id = j.job_id
+inner join hr.departments d on jh.department_id = d.department_id
+group by e.email, `nome completo`, departamento, cargo
+having e.email = email_emp
+order by departamento, cargo;
+end $$
