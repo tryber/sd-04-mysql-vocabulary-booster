128da4fda406a3bc478d8a6b9384e972200ab8ff
djonata-dias <diasdjonata@gmail.com>
2020-09-07 21:51:35 -0300

    Desafio 17 Teste 8

--- desafio17.sql
+++ desafio17.sql
@@ -1,39 +1,10 @@
 SELECT
-    ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) 'Anos trabalhados',
-    CONCAT(
-        IF(
-            LENGTH(DAY(jh.START_DATE)) = 1,
-            CONCAT('0', DAY(jh.START_DATE)),
-            DAY(jh.START_DATE)
-        ),
-        '/',
-        IF(
-            LENGTH(MONTH(jh.START_DATE)) = 1,
-            CONCAT('0', MONTH(jh.START_DATE)),
-            MONTH(jh.START_DATE)
-        ),
-        '/',
-        YEAR(jh.START_DATE)
-    ) 'Data de início',
-    CONCAT(
-        IF(
-            LENGTH(DAY(jh.END_DATE)) = 1,
-            CONCAT('0', DAY(jh.END_DATE)),
-            DAY(jh.END_DATE)
-        ),
-        '/',
-        IF(
-            LENGTH(MONTH(jh.END_DATE)) = 1,
-            CONCAT('0', MONTH(jh.END_DATE)),
-            MONTH(jh.END_DATE)
-        ),
-        '/',
-        YEAR(jh.END_DATE)
-    ) 'Data de rescisão',
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo'
+    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) 'Nome completo',
+    DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') 'Data de início',
+    DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') 'Data de rescisão',
 FROM
     hr.job_history jh
     INNER JOIN hr.employees e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 ORDER BY
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) ASC,
-    (YEAR(jh.END_DATE) - YEAR(jh.START_DATE)) ASC;
+    ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) 'Anos trabalhados';
