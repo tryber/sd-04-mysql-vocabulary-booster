1adee52f7de64372960c9adf23d08c5cbff6769d
binhus <eng.robsonwilli@gmail.com>
2020-09-08 18:25:54 -0300

    desafio 7 corrigido

--- desafio7.sql
+++ desafio7.sql
@@ -4,7 +4,7 @@ SELECT
   ) AS "Nome completo",
   jobs.JOB_TITLE AS "Cargo",
   DATE(history.START_DATE) AS "Data de início",
-  employees.SALARY AS "Salário	"
+  employees.SALARY AS "Salário"
 FROM
   hr.job_history AS history
   INNER JOIN hr.jobs AS jobs ON (history.JOB_ID = jobs.JOB_ID)
