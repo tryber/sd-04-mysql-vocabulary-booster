23b973fd8d704febb170c0532f9cee58724637d4
felipedoss <felipecaetano25.1996@gmail.com>
2020-09-08 09:47:59 -0300

    add desafio12

---
+++ desafio12.sql
@@ -0,0 +1,6 @@
+select concat(E1.FIRST_NAME, ' ', E1.LAST_NAME) `Nome completo funcionário 1`,
+E1.SALARY `Salário funcionário 1`,  E1.PHONE_NUMBER `Telefone funcionário 1`,
+concat(E2.FIRST_NAME, ' ', E2.LAST_NAME) `Nome completo funcionário 2`, E2.SALARY `Salário funcionário 2`, E2.PHONE_NUMBER `Telefone funcionário 2`
+from hr.employees E1
+inner join hr.employees E2 on E1.JOB_ID = E2.JOB_ID and  E1.EMPLOYEE_ID <> E2.EMPLOYEE_ID
+order by `Nome completo funcionário 1`, `Nome completo funcionário 2`;
