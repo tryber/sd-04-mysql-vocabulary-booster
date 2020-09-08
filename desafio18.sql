1458b4a9805022815f13ad0af59aeef9f16eded3
djonata-dias <diasdjonata@gmail.com>
2020-09-08 11:52:29 -0300

    Desafio 17 Teste 11

--- desafio18.sql
+++ desafio18.sql
@@ -3,12 +3,15 @@ USE hr;
 DROP FUNCTION IF EXISTS exibir_quantidade_pessoas_contratadas_por_mes_e_ano;
 
 
-DELIMITER $ $ CREATE PROCEDURE exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano INT) BEGIN
+DELIMITER $$ 
+CREATE PROCEDURE exibir_quantidade_pessoas_contratadas_por_mes_e_ano  (mes INT, ano INT) BEGIN
 SELECT
     COUNT(*)
 FROM
     hr.job_history AS jh
 WHERE
-    MONTH(jh.STAR_DATE) = mes
-    AND YEAR(jh.STAR_DATE) = ano
-END $ $ DELIMITER;
+    MONTH(jh.START_DATE) = mes
+    AND YEAR(jh.START_DATE) = ano;
+END $$ 
+
+DELIMITER ;
