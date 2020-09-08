6f416a8a79dc8898648232f11b32baf44b78bf60
felipedoss <felipecaetano25.1996@gmail.com>
2020-09-08 11:49:38 -0300

    update desafio4

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,11 @@
-SELECT JOB_TITLE as Cargo, (MAX_SALARY - MIN_SALARY) as `Variação Salarial`,
-round((MIN_SALARY / 12), 2) as `Média mínima mensal`, round((MAX_SALARY / 12), 2) as `Média máxima mensal`
-from hr.jobs
-order by `Variação Salarial`, Cargo;
+select j.JOB_TITLE as `Cargo`, ROUND(AVG(SALARY), 2) as `Média salarial`,
+  case
+    when ROUND(AVG(SALARY), 2) >= 2000 and ROUND(AVG(SALARY), 2) <= 5800 then 'Júnior'
+    when ROUND(AVG(SALARY), 2) >= 5801 and ROUND(AVG(SALARY), 2) <= 7500 then 'Pleno'
+    when ROUND(AVG(SALARY), 2) >= 7501 and ROUND(AVG(SALARY), 2) <= 10500 then 'Sênior'
+    when ROUND(AVG(SALARY), 2) > 10500 then 'CEO'
+  end as `Senioridade` 
+from hr.employees as e
+inner join jobs as j on e.JOB_ID = j.JOB_ID
+group by `Cargo` 
+order by `Média salarial` asc, `Cargo` asc;
