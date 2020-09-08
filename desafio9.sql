4196d75d97d77d07226233c3bc62eaf8c974a2dd
binhus <eng.robsonwilli@gmail.com>
2020-09-08 18:06:52 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+  CONCAT(employees.FirstName, " ", employees.LastName) AS "Nome completo",
+  COUNT(*) AS "Total de pedidos"
+FROM
+  w3schools.orders as orders
+  INNER JOIN w3schools.employees AS employees ON (employees.EmployeeID = orders.CustomerID)
+GROUP BY
+  `Nome completo`
+ORDER BY
+  `Total de pedidos`;
