e86aad69a2fafdfa55f93f9d45e76d23d40c979a
felipedoss <felipecaetano25.1996@gmail.com>
2020-09-08 15:58:02 -0300

    add

--- desafio17.sql
+++ desafio17.sql
@@ -1,8 +1,8 @@
-select concat(e.first_name, ' ', e.last_name) as `nome completo`,
-  date_format(jh.start_date, "%d/%m/%y") as 'data de início',
-  date_format(jh.end_date, "%d/%m/%y") as 'data de rescisão',
-  round(datediff(jh.end_date, jh.start_date) / 365, 2) as `anos trabalhados`
+select concat(e.first_name, ' ', e.last_name) as `Nome completo`,
+  date_format(jh.start_date, "%d/%m/%y") as 'Data de início',
+  date_format(jh.end_date, "%d/%m/%y") as 'Data de rescisão',
+  round(datediff(jh.end_date, jh.start_date) / 365, 2) as `Anos trabalhados`
   from hr.job_history as jh
   inner join hr.employees as e
   on jh.employee_id = e.employee_id
-  order by `nome completo`, `anos trabalhados`;
+  order by `Nome completo`, `Anos trabalhados`;
