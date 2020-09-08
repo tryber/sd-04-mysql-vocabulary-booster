e82ec7abcee5c87f70b31b975f6d17b090e45b6b
Victor L <victor.lustosacp@gmail.com>
2020-09-08 16:04:50 -0300

    desafios 12

---
+++ desafio11.sql
@@ -0,0 +1,8 @@
+SELECT  c.ContactName Nome,
+c.Country País, COUNT(*) - 1 `Número de compatriotas`
+FROM w3schools.customers c, w3schools.customers c2
+WHERE c.Country = c2.Country
+GROUP BY Nome,
+País
+HAVING `Número de compatriotas` > 0
+ORDER BY Nome;
