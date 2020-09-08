c91eabb295de9074bd64c8bcb1f1df0b9b9335bd
Victor L <victor.lustosacp@gmail.com>
2020-09-08 16:22:25 -0300

    cc

--- desafio17.sql
+++ desafio17.sql
@@ -4,4 +4,4 @@ DATE_FORMAT(end_date, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(datediff(end_date, start_date) / 365, 2) AS 'Anos trabalhados'
 FROM hr.job_history AS j
 JOIN hr.employees AS e ON e.employee_id = j.employee_id
-ORDER BY `Nome completo`, `Anos trabalhados`
+ORDER BY `Nome completo`, `Anos trabalhados`;
