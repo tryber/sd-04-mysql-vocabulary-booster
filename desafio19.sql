083bdfb9836e3ede5c99e89bef9cae36d87fae08
felipedoss <felipecaetano25.1996@gmail.com>
2020-09-10 15:52:12 -0300

    update 19

--- desafio19.sql
+++ desafio19.sql
@@ -5,7 +5,7 @@ select concat(e.first_name, ' ', e.last_name) `nome completo`, d.department_name
 from hr.employees e 
 inner join hr.job_history jh on jh.employee_id = e.employee_id
 inner join hr.jobs j on jh.job_id = j.job_id
-inner join hr.departments d on jh.department_id = d.department_i
+inner join hr.departments d on jh.department_id = d.department_id
 where e.email = email_emp
 order by departamento, cargo;
 end $$
