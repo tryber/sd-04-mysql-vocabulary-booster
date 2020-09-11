bf1af2844b8c09b075f85cf9b02e70cdb3a2966d
saturnian99 <saturnian.dsg@gmail.com>
2020-09-11 18:31:43 -0300

    Fixing Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -5,7 +5,7 @@ Country AS País,
 SELECT COUNT(*) - 1
 FROM w3schools.customers
 WHERE Country = c.Country
-) AS `Números de compatriotas`
+) AS `Número de compatriotas`
 FROM w3schools.customers AS c
 HAVING `Número de compatriotas` > 0
 ORDER BY Nome;
