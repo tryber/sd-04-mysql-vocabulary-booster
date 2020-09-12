6d9980243371f17bed76593db805b8be1d65b11e
saturnian99 <saturnian.dsg@gmail.com>
2020-09-12 13:53:41 -0300

    Fixing Desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job_title VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-    FROM hr.employees AS e
-    JOIN hr.jobs AS j
-    ON j.JOB_ID = e.JOB_ID
-    WHERE j.JOB_TITLE = job_title;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+FROM hr.employees AS e
+JOIN hr.jobs AS j
+ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = job_title;
 END $$
 
 DELIMITER ;
